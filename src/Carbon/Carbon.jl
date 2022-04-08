81d7000eb5f8ea1e8fdc6cbd38cd354a9345a0db
kmdeck <kdeck@caltech.edu>
2022-04-08 16:55:33 -0700

    fixed RHS to not use parent, but initializing is hard

--- src/Carbon/Carbon.jl
+++ src/Carbon/Carbon.jl
@@ -1,10 +1,11 @@
 module Carbon
 using DocStringExtensions
 using LinearAlgebra
+using StaticArrays
 
 using ClimaLSM
 using ClimaLSM.Domains
-import ClimaLSM: name, make_rhs, prognostic_vars
+import ClimaLSM: name, make_rhs, prognostic_vars, prognostic_types
 import ClimaLSM.Domains: coordinates
 
 export BulkNPools, Clima2PoolDefault, PrescribedNPPModel
@@ -49,8 +50,11 @@ A bulk carbon model with N pools.
 # Fields
 $(DocStringExtensions.FIELDS)
 """
-struct BulkNPools{FT} <: AbstractCarbonModel{FT}
-    "The number of carbon poolsN"
+struct BulkNPools{FT, D <: AbstractDomain, NT <: AbstractNPPModel} <:
+       AbstractCarbonModel{FT}
+    "The domain"
+    domain::D
+    "The number of carbon pools N"
     number_of_pools::Int
     "The names of the pools"
     labels::Vector{String}
@@ -63,7 +67,7 @@ struct BulkNPools{FT} <: AbstractCarbonModel{FT}
     "The transfer matrix between pools, of size NxN (unitless)"
     transfer_matrix::Matrix{FT}
     "The NPP model"
-    NPP::AbstractNPPModel{FT}
+    NPP::NT
 end
 
 """
@@ -80,16 +84,15 @@ Returns the prognostic variable names of the bulk carbon model.
 """
 ClimaLSM.prognostic_vars(::BulkNPools) = (:pool,)
 
-"""
-    ClimaLSM.Domains.coordinates(model::BulkNPools{FT})
 
-A stand-in for initializing the carbon prognostic state, which is
-an array at each coordinate point.
-
-Currently, we only support one array/one coordinate point.
 """
-ClimaLSM.Domains.coordinates(model::BulkNPools{FT}) where {FT} =
-    FT.(zeros(model.number_of_pools))
+    ClimaLSM.prognostic_types(model::BulkNPools{FT}) where {FT}
+
+Returns the prognostic variable types of the bulk carbon model.
+"""
+function ClimaLSM.prognostic_types(m::BulkNPools{FT}) where {FT}
+    return (SVector{m.number_of_pools, FT},)
+end
 
 """
     ClimaLSM.make_rhs(model::BulkNPools{FT}) where {FT}
@@ -99,13 +102,17 @@ Returns the rhs! function for the bulk carbon model.
 function ClimaLSM.make_rhs(model::BulkNPools{FT}) where {FT}
     function rhs!(dY, Y, p, t)
         NPP = compute_NPP(model.NPP, Y, p, t)
-        external_input = model.allocation_fraction * NPP
-        dY.carbon.pool .=
-            external_input .+
-            model.transfer_matrix * (
-                model.environmental_limitations ./ model.turnover_time .*
-                Y.carbon.pool
+        external_input::Scalar{Vector{FT}} =
+            Scalar(model.allocation_fraction * NPP)
+        N::Int = model.number_of_pools
+        N2::Int = N * N
+        matrix::SMatrix{N, N, FT, N2} =
+            SMatrix{model.number_of_pools, model.number_of_pools}(
+                model.transfer_matrix *
+                Diagonal(model.environmental_limitations) *
+                Diagonal(1.0 ./ model.turnover_time),
             )
+        dY.carbon.pool = map(x -> matrix * x, Y.carbon.pool) .+ external_input
     end
     return rhs!
 end
@@ -115,14 +122,19 @@ end
 
 Returns the default Clima 2 pool bulk carbon model.
 """
-function Clima2PoolDefault(::Type{FT}, NPP::AbstractNPPModel{FT}) where {FT}
+function Clima2PoolDefault(
+    ::Type{FT},
+    domain::D,
+    NPP::NT,
+) where {FT, D <: AbstractDomain{FT}, NT <: AbstractNPPModel{FT}}
     labels = ["fast", "slow"]
     turnover_time = [10.0, 100.0] # years
     environmental_limitations = [1.0, 1.0]# making dependent on T, θ
     allocation_fraction = [1.0, 0.0] # constant or not
     respired_fraction = 0.75 # unitless, model for this?
     transfer_matrix = [-1.0 0.0; (1.0-respired_fraction) -1.0] # unitless
-    return BulkNPools{FT}(
+    return BulkNPools{FT, typeof(domain), typeof(NPP)}(
+        domain,
         2,
         labels,
         turnover_time,
--- test/Carbon/eq_test.jl
+++ test/Carbon/eq_test.jl
@@ -6,17 +6,24 @@ if !("." in LOAD_PATH)
 end
 using ClimaLSM
 using ClimaLSM.Carbon
+using ClimaLSM.Domains
 
 @testset "Carbon equlibrium" begin
+    test_domain = Column(; zlim = (-1.0, 0.0), nelements = 3)
     NPP_model =
         PrescribedNPPModel{Float64}((t) -> 60 + 5 * sin(2 * pi * t / 10)) # PgC/year
-    carbon = Clima2PoolDefault(Float64, NPP_model)
+    carbon = Clima2PoolDefault(Float64, test_domain, NPP_model)
     Y, p, coords = initialize(carbon)
 
-    Y.carbon.pool[1] = 600.0
-    Y.carbon.pool[2] = 1500.0
+    #= how do you intialize? why is this so hard? 
+    function init_carbon(z::FT)::SVector{2, FT} where {FT<:AbstractFloat}
+        return SVector{2, FT}([600.0, 1500.0])
+    end
+
+    Y.carbon.pool = init_carbon.(coords.z)
+    =#
     ode_function! = make_ode_function(carbon)
     dY = similar(Y)
     ode_function!(dY, Y, p, 0.0)
-    @test sum(abs.(dY.carbon.pool .- [0.0, 0.0])) < 1e-14
+    @test parent(dY.carbon.pool) == [60.0 0.0; 60.0 0.0; 60.0 0.0]
 end
