f41c497a12f91725ff23a9cd7ba8d563285f3bd8
kmdeck <kdeck@caltech.edu>
2022-05-13 16:40:34 -0700

    it runs but the computed Jac doesnt match autodiff Jac

--- examples/richards_implicit.jl
+++ examples/richards_implicit.jl
@@ -1,11 +1,16 @@
 using LinearAlgebra
+using OrdinaryDiffEq
 
 using ClimaCore: Operators, Geometry, Spaces, Fields
 using ClimaCore.Utilities: half
+if !("." in LOAD_PATH)
+    push!(LOAD_PATH, ".")
+end
 using ClimaLSM
-using ClimaLSM.Soil: RichardsModel, RichardsParameters, FluxBC
+using ClimaLSM.Soil
 using ClimaLSM.Domains: HybridBox, Column
-using ClimaCore.Utilities: half
+include("ordinary_diff_eq_bug_fixes.jl")
+include("autodiff_test.jl")
 using UnPack
 const compose = Operators.ComposeStencils()
 const apply = Operators.ApplyStencil()
@@ -19,7 +24,7 @@ Base.one(::Type{T}) where {T′, A, S, T <: Geometry.AxisTensor{T′, 1, A, S}} 
 FT = Float64
 gradc2f_op = Operators.GradientC2F(
     bottom = Operators.SetValue(FT(0.0)),
-    top = Operators.SetValue(FT(0.0))
+    top = Operators.SetValue(FT(0.0)),
 )
 gradc2f_stencil = Operators.Operator2Stencil(gradc2f_op)
 divf2c_op = Operators.DivergenceF2C(
@@ -33,111 +38,154 @@ interpc2f_op = Operators.InterpolateC2F(
 )
 interpc2f_stencil = Operators.Operator2Stencil(interpc2f_op)
 
-struct TridiagonalJacobian{R, J1}
+struct TridiagonalJacobian{R, J1, J2}
     # reference to dtγ, which is specified by the ODE solver
     dtγ_ref::R
     ∂ϑt∂ϑ::J1
+    ∂ϑt∂ϑ_column_array::J2
     flag::Symbol
+    test_flag::Bool
 end
 
-function TridiagonalJacobian(Y, flag::Symbol)
+function TridiagonalJacobian(Y, flag::Symbol; test_flag::Bool = false)
     FT = eltype(Y.soil.ϑ_l)
     dtγ_ref = Ref(zero(FT))
     space = axes(Y.soil.ϑ_l)
+    N = Spaces.nlevels(space)
     Jtype = Operators.StencilCoefs{-1, 1, NTuple{3, FT}}
     J = Fields.Field(Jtype, space)
-    args = (dtγ_ref,J)
-    return TridiagonalJacobian{typeof.(args)...}(args..., flag)
+    column_array = Tridiagonal(
+        Array{FT}(undef, N - 1),
+        Array{FT}(undef, N),
+        Array{FT}(undef, N - 1),
+    )
+    args = (dtγ_ref, J, column_array)
+    return TridiagonalJacobian{typeof.(args)...}(args..., flag, test_flag)
 end
 Base.similar(w::TridiagonalJacobian) = w
-to_scalar_coefs(vector_coefs) =
-    map(vector_coef-> vector_coef.u₃, vector_coefs)
+to_scalar_coefs(vector_coefs) = map(vector_coef -> vector_coef.u₃, vector_coefs)
 function make_Wfact(model::RichardsModel)
     function Wfact!(W, Y, p, dtγ, t)
-        (; dtγ_ref, ∂ϑt∂ϑ, flag) = W
+        (; dtγ_ref, ∂ϑt∂ϑ, flag, test_flag) = W
         dtγ_ref[] = dtγ
         # ∂ϑ∂t = ϑt = div(I(K(θ)) grad(h(θ)))
         if flag == :exact
             @. ∂ϑt∂ϑ = compose(
-                divf2c_stencil(one.(gradc2f_op.(p.soil.ψ + model.coordinates.z))),
+                divf2c_stencil(
+                    one.(gradc2f_op.(p.soil.ψ + model.coordinates.z)),
+                ),
                 to_scalar_coefs(
-                    interpc2f_op(dKdθ((Y.soil.ϑ_l - θ_r)/(ν-θ_r),
-                                      ν,
-                                      θ_r,
-                                      vg_m,
-                                      K_sat
-                                      )
-                                 )*interpc2f_stencil(one(Y.soil.ϑ_l))*
+                    interpc2f_op(dKdθ(Y.soil.ϑ_l, ν, θ_r, vg_m, K_sat)) *
+                    interpc2f_stencil(one(Y.soil.ϑ_l)) *
                     gradc2f_op(p.soil.ψ + model.coordinates.z) +
-                    interpc2f_op(p.soil.K*dψdθ((Y.soil.ϑ_l - θ_r)/(ν-θ_r),
-                                               ν,
-                                               θ_r,
-                                               vg_α,
-                                               vg_n,
-                                               vg_m,
-                                               S_s
-                                               )
-                                 )* gradc2f_stencil(one.(Y.soil.ϑ_l))
-                )
+                    interpc2f_op(
+                        p.soil.K *
+                        dψdθ(Y.soil.ϑ_l, ν, θ_r, vg_α, vg_n, vg_m, S_s),
+                    ) * gradc2f_stencil(one.(Y.soil.ϑ_l)),
+                ),
             )
         elseif flag == :modifiedpicard
             @. ∂ϑt∂ϑ = compose(
-                divf2c_stencil(one.(gradc2f_op.(p.soil.ψ + model.coordinates.z))),
+                divf2c_stencil(
+                    one.(gradc2f_op.(p.soil.ψ + model.coordinates.z)),
+                ),
                 to_scalar_coefs(
-                    interpc2f_op(p.soil.K*dψdθ((Y.soil.ϑ_l-θ_r)/(ν-θ_r),
-                                               ν,
-                                               θ_r,
-                                               vg_α,
-                                               vg_n,
-                                               vg_m,
-                                               S_s)
-                                 )*gradc2f_stencil(one.(Y.soil.ϑ_l))
-                )
+                    interpc2f_op(
+                        p.soil.K *
+                        dψdθ(Y.soil.ϑ_l, ν, θ_r, vg_α, vg_n, vg_m, S_s),
+                    ) * gradc2f_stencil(one.(Y.soil.ϑ_l)),
+                ),
             )
         else
             println("Unsupported Jacobian approximation")
         end
-        
+        i, j, h = 1, 1, 1
+
+        if test_flag
+            args = (implicit_tendency!, Y, p, t, i, j, h)
+            display(matrix_column(∂ϑt∂ϑ, axes(Y.soil.ϑ_l), i, j, h))
+            display(
+                exact_column_jacobian_block(
+                    args...,
+                    (:soil, :ϑ_l),
+                    (:soil, :ϑ_l),
+                ),
+            )
+
+            @assert matrix_column(∂ϑt∂ϑ, axes(Y.soil.ϑ_l), i, j, h) ≈
+                    exact_column_jacobian_block(
+                args...,
+                (:soil, :ϑ_l),
+                (:soil, :ϑ_l),
+            )
+        end
 
         # CLM approach
         # Newton's method with one iteration, and use the previous time step as your first guess.
-        
+
     end
     return Wfact!
 end
-#S = (θ - θ_r) / (ν - θ_r), but the deriv is with respect to θ
-function dKdθ(S::FT, ν::FT, θ_r::FT, vg_m::FT, Ksat::FT)::FT where {FT}
+
+function dKdθ(θ, ν, θ_r, vg_m, Ksat)
+    S = (θ - θ_r) / (ν - θ_r)
     if S < 1
-        return (((1.0 - (1.0 - S^(1.0 / vg_m))^vg_m))^2.0 / 2.0 / S^0.5
-                + 2 * S^(1 / vg_m - 1 / 2) * (1.0 - (1.0 - S^(1.0 / vg_m))^vg_m) / (1 - S^(1 / vg_m))^(1.0 - vg_m)
-                )* K_sat / (ν - θ_r)
+        f = 1.0 - (1.0 - S^(1.0 / vg_m))^vg_m
+        f1 = f^2.0 / 2.0 / S^0.5
+        f2 = 2 * S^(1 / vg_m - 1 / 2) * f / (1 - S^(1 / vg_m))^(1.0 - vg_m)
+        return (f1 + f2) * Ksat / (ν - θ_r)
     else
-        return 0.0
+        return zero(typeof(S))
     end
-    
+
 end
-#S = (θ - θ_r) / (ν - θ_r), but the deriv is with respect to θ
-function dψdθ(S::FT,  ν::FT, θ_r::FT, vg_α::FT, vg_n::FT, vg_m::FT, S_s::FT)::FT where {FT}
+#    if S < 1
+#        return (((1 - (1 - S^(1 / vg_m))^vg_m))^2 / 2 / S^0.5
+#                + 2 * S^(1 / vg_m - 1 / 2) * (1 - (1 - S^(1 / vg_m))^vg_m) / (1 - S^(1 / vg_m))^(1 - vg_m)
+#                )* Ksat / (ν - θ_r)
+#    else
+#        return zero(typeof(S))
+#    end
+
+#end
+
+function dψdθ(θ, ν, θ_r, vg_α, vg_n, vg_m, S_s)
+    S = (θ - θ_r) / (ν - θ_r)
     if S < 1.0
         return 1.0 / (vg_α * vg_m * vg_n) / (ν - θ_r) *
-            (S^(-1 / vg_m) - 1)^(1 / vg_n - 1) *
-            S^(-1 / vg_m - 1)
+               (S^(-1 / vg_m) - 1)^(1 / vg_n - 1) *
+               S^(-1 / vg_m - 1)
     else
         return 1.0 / S_s
     end
 end
 
+#    if S < 1
+#        return 1 / (vg_α * vg_m * vg_n) / (ν - θ_r) *
+#            (S^(-1 / vg_m) - 1)^(1 / vg_n - 1) *
+#            S^(-1 / vg_m - 1)
+#    else
+#        return 1 / S_s
+#    end
+#end
+
 
 function make_implicit_tendency(model::RichardsModel)
-    function implicit_tendency!(dY,Y,p,t)
+    function implicit_tendency!(dY, Y, p, t)
         @unpack ν, vg_α, vg_n, vg_m, K_sat, S_s, θ_r = model.parameters
         # when does aux get updated?
-        @. p.soil.K = hydraulic_conductivity(
+        (; K, ψ) = p.soil
+        if eltype(Y) <: Dual
+            K = similar(Y.soil.ϑ_l)
+            ψ = similar(Y.soil.ϑ_l)
+        end
+        @. K = hydraulic_conductivity(
             K_sat,
             vg_m,
             effective_saturation(ν, Y.soil.ϑ_l, θ_r),
         )
-        @. p.soil.ψ = pressure_head(vg_α, vg_n, vg_m, θ_r, Y.soil.ϑ_l, ν, S_s)
+        @. ψ = pressure_head(vg_α, vg_n, vg_m, θ_r, Y.soil.ϑ_l, ν, S_s)
+
         top_flux_bc, bot_flux_bc =
             boundary_fluxes(model.boundary_conditions, p, t)
         z = model.coordinates.z
@@ -147,15 +195,14 @@ function make_implicit_tendency(model::RichardsModel)
             top = Operators.SetValue(Geometry.WVector(top_flux_bc)),
             bottom = Operators.SetValue(Geometry.WVector(bot_flux_bc)),
         )
-        @. dY.soil.ϑ_l =
-            -(divf2c_water(-interpc2f(p.soil.K) * gradc2f_water(p.soil.ψ + z)))
+        @. dY.soil.ϑ_l = -(divf2c_water(-interpc2f(K) * gradc2f_water(ψ + z)))
     end
     return implicit_tendency!
 end
 
 
 function make_explicit_tendency(model::RichardsModel)
-    function explicit_tendency!(dY,Y,p,t)
+    function explicit_tendency!(dY, Y, p, t)
         #when does aux get updated?
         @. p.soil.K = hydraulic_conductivity(
             K_sat,
@@ -165,19 +212,41 @@ function make_explicit_tendency(model::RichardsModel)
         @. p.soil.ψ = pressure_head(vg_α, vg_n, vg_m, θ_r, Y.soil.ϑ_l, ν, S_s)
         hdiv = Operators.WeakDivergence()
         hgrad = Operators.Gradient()
-        @. dY.soil.ϑ_l += -hdiv(-p.soil.K * hgrad(p.soil.ψ + model.coordinates.z))
+        @. dY.soil.ϑ_l +=
+            -hdiv(-p.soil.K * hgrad(p.soil.ψ + model.coordinates.z))
         Spaces.weighted_dss!(dY.soil.ϑ_l)
     end
     return explicit_tendency!
 end
-#domain = HybridBox(;
-#                   zlim = (0.0,1.0),
-#                   xlim = (0.0,100.0),
-#                   ylim = (0.0,100.0),
-#                   nelements = (10,10,10),
-#                   npolynomial = 1,
-#                   periodic = (true,true)
-#                   )
+
+function linsolve!(::Type{Val{:init}}, f, u0; kwargs...)
+    function _linsolve!(x, A, b, update_matrix = false; kwargs...)
+        # TODO: Do this with stencil_solve!.
+        ϑ_l = x.soil.ϑ_l
+        (; ∂ϑt∂ϑ, ∂ϑt∂ϑ_column_array) = A
+        Ni, Nj, _, _, Nh = size(Spaces.local_geometry_data(axes(ϑ_l)))
+        for h in 1:Nh, j in 1:Nj, i in 1:Ni
+            ϑ_column_view = parent(Spaces.column(ϑ_l, i, j, h))
+            ∂ϑt∂ϑ_column = Spaces.column(∂ϑt∂ϑ, i, j, h)
+            @views ∂ϑt∂ϑ_column_array.dl .= parent(∂ϑt∂ϑ_column.coefs.:1)[2:end]
+            ∂ϑt∂ϑ_column_array.d .= parent(∂ϑt∂ϑ_column.coefs.:2)
+            @views ∂ϑt∂ϑ_column_array.du .=
+                parent(∂ϑt∂ϑ_column.coefs.:3)[1:(end - 1)]
+            ldiv!(lu!(∂ϑt∂ϑ_column_array), ϑ_column_view)
+        end
+    end
+end
+
+
+
+domain = HybridBox(;
+    zlim = (-10.0, 0.0),
+    xlim = (0.0, 100.0),
+    ylim = (0.0, 100.0),
+    nelements = (10, 10, 10),
+    npolynomial = 1,
+    periodic = (true, true),
+)
 ν = FT(0.495)
 K_sat = FT(0.0443 / 3600 / 100) # m/s
 S_s = FT(1e-3) #inverse meters
@@ -185,11 +254,8 @@ vg_n = FT(2.0)
 vg_α = FT(2.6) # inverse meters
 vg_m = FT(1) - FT(1) / vg_n
 θ_r = FT(0)
-zmax = FT(0)
-zmin = FT(-10)
-nelems = 10
 
-soil_domain = Column(; zlim = (zmin, zmax), nelements = nelems)
+soil_domain = domain
 top_flux_bc = FT(0.0)
 bot_flux_bc = FT(0.0)
 sources = ()
@@ -197,11 +263,11 @@ boundary_conditions = FluxBC{FT}(top_flux_bc, bot_flux_bc)
 params = RichardsParameters{FT}(ν, vg_α, vg_n, vg_m, K_sat, S_s, θ_r)
 
 soil = RichardsModel{FT}(;
-                              parameters = params,
-                              domain = soil_domain,
-                              boundary_conditions = boundary_conditions,
-                              sources = sources,
-                              )
+    parameters = params,
+    domain = soil_domain,
+    boundary_conditions = boundary_conditions,
+    sources = sources,
+)
 
 Y, p, coords = initialize(soil)
 function init_soil!(Y, z, params)
@@ -223,6 +289,33 @@ init_soil!(Y, coords.z, soil.parameters)
 update_aux! = make_update_aux(soil)
 update_aux!(p, Y, 0.0)
 
-W = TridiagonalJacobian(Y, :exact)
+W = TridiagonalJacobian(Y, :exact; test_flag = true)
 Wfact! = make_Wfact(soil)
+t_start = 0.0
+t_end = 100.0
+dt = 10.0
+implicit_tendency! = make_implicit_tendency(soil)
+explicit_tendency! = make_explicit_tendency(soil)
+jac_kwargs = (; jac_prototype = W, Wfact = Wfact!)
+alg_kwargs = (; linsolve = linsolve!)
+problem = SplitODEProblem(
+    ODEFunction(
+        implicit_tendency!;
+        jac_kwargs...,
+        tgrad = (∂Y∂t, Y, p, t) -> (∂Y∂t .= FT(0)),
+    ),
+    explicit_tendency!,
+    Y,
+    (t_start, t_end),
+    p,
+)
+integrator = init(
+    problem,
+    Rosenbrock23(; alg_kwargs...);
+    dt = dt,
+    adaptive = false,
+    progress = true,
+    progress_steps = 1,
+)
 
+sol = @timev OrdinaryDiffEq.solve!(integrator)
--- src/Soil/soil_hydrology_parameterizations.jl
+++ src/Soil/soil_hydrology_parameterizations.jl
@@ -12,13 +12,8 @@ export volumetric_liquid_fraction,
 A pointwise function returning the volumetric liquid fraction
 given the augmented liquid fraction and the effective porosity.
 """
-function volumetric_liquid_fraction(ϑ_l::FT, ν_eff::FT) where {FT}
-    if ϑ_l < ν_eff
-        θ_l = ϑ_l
-    else
-        θ_l = ν_eff
-    end
-    return θ_l
+function volumetric_liquid_fraction(ϑ_l, ν_eff::FT) where {FT}
+    return min(ϑ_l, ν_eff)
 end
 
 
@@ -28,7 +23,7 @@ end
 A point-wise function returning the matric potential, using the
 van Genuchten formulation.
 """
-function matric_potential(α::FT, n::FT, m::FT, S::FT) where {FT}
+function matric_potential(α::FT, n::FT, m::FT, S) where {FT}
     ψ_m = -((S^(-FT(1) / m) - FT(1)) * α^(-n))^(FT(1) / n)
     return ψ_m
 end
@@ -38,7 +33,7 @@ end
 
 A point-wise function computing the effective saturation.
 """
-function effective_saturation(porosity::FT, ϑ_l::FT, θr::FT) where {FT}
+function effective_saturation(porosity::FT, ϑ_l, θr::FT) where {FT}
     ϑ_l_safe = max(ϑ_l, θr + eps(FT))
     S_l = (ϑ_l_safe - θr) / (porosity - θr)
     return S_l
@@ -66,7 +61,7 @@ function pressure_head(
     n::FT,
     m::FT,
     θ_r::FT,
-    ϑ_l::FT,
+    ϑ_l,
     ν_eff::FT,
     S_s::FT,
 ) where {FT}
@@ -85,11 +80,11 @@ end
 A point-wise function returning the hydraulic conductivity, using the
 van Genuchten formulation.
 """
-function hydraulic_conductivity(K_sat::FT, m::FT, S::FT) where {FT}
-    if S < FT(1)
-        K = sqrt(S) * (FT(1) - (FT(1) - S^(FT(1) / m))^m)^FT(2)
+function hydraulic_conductivity(K_sat, m, S::T) where {T}
+    if S < 1
+        K = sqrt(S) * (1 - (1 - S^(1 / m))^m)^2
     else
-        K = FT(1)
+        K = T(1)
     end
     return K * K_sat
 end
