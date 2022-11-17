4f112e4537f0b17a074305a53cbe5718e997c0d8
AlexisRenchon <a.renchon@gmail.com>
2022-11-17 10:01:24 -0500

    Cmic Cr Csom functions

--- experiment/SoilCO2/DETECTModel_run.jl
+++ experiment/SoilCO2/DETECTModel_run.jl
@@ -15,7 +15,7 @@ using ClimaLSM.DETECT: DETECTModel, DETECTParameters, RootProduction, MicrobePro
 FT = Float32
 include("./DETECTModel_params.jl")
 nelems = 50 # number of layers in the vertical
-zmin = FT(-5)
+zmin = FT(-1) # 0 to 1 m depth
 zmax = FT(0.0)
 soil_domain = Column(; zlim = (zmin, zmax), nelements = nelems)
 top_bc = SoilCO2StateBC{FT}((p, t) -> FT(3000.0))
@@ -24,14 +24,20 @@ sources = (RootProduction(),MicrobeProduction())
 boundary_conditions = (; CO2 = (top = top_bc, bottom = bot_bc))
 params = DETECTParameters{FT}(Pz, Rᵦ, α₁ᵣ, α₂ᵣ, α₃ᵣ, Sᶜ, Mᶜ, Vᵦ, α₁ₘ, α₂ₘ, α₃ₘ, Kₘ, CUE, pf, Dₗᵢ, E₀ₛ, T₀, α₄, Tᵣₑ,	α₅, BD, ϕ₁₀₀, PD, Dstp, P₀, b)
 
-θ(t, z) = FT(0.3) #exp(-z)*sin(t)*100 # for now
+
+θ(t, z) = FT(0.3) # exp(-z)*sin(t)*100 # for now
+
+# plt = lineplot([cos, sin], -π/2, 2π)
+# Tₛ(t) = 10*sin(.0001t) + 290
+# plt = lineplot(Tₛ, 0, 86400) # 86400 seconds in a day...
+
 Tₛ(t, z) = FT(303.0) #exp(-z)*sin(t) #
 θₐᵣ(t, z) = FT(0.3) #exp(-z)*sin(t) #
 θₐₘ(t, z) = FT(0.3) #exp(-z)*sin(t) #
 Tₛₐ(t, z) = FT(303.0) #exp(-z)*sin(t)*100 + 273
-Cᵣ(t, z) = FT(10.0)
-Csom(t, z) = FT(5.0)
-Cmic(t, z) = FT(1.0)
+Cᵣ(t, z) = FT(15.0) # FT(15.0*(z+1)/1)
+Csom(t, z) = FT(25.0) # FT(25.0*(z+1)/1)
+Cmic(t, z) = FT(1.0) # FT(1.0*(z+1)/1)
 
 soil_drivers = PrescribedSoil(Tₛ, θ, Tₛₐ, θₐᵣ, θₐₘ, Cᵣ, Csom, Cmic)
 model = DETECTModel{FT}(; parameters = params, domain = soil_domain, sources = sources, boundary_conditions= boundary_conditions, drivers = soil_drivers)
@@ -45,7 +51,7 @@ function init_DETECT!(Y, z, params)
 		params::DETECTParameters{FT},
 	) where {FT}
 		@unpack Pz, Rᵦ, α₁ᵣ, α₂ᵣ, α₃ᵣ, Sᶜ, Mᶜ, Vᵦ, α₁ₘ, α₂ₘ, α₃ₘ, Kₘ, CUE, pf, Dₗᵢ, E₀ₛ, T₀, α₄, Tᵣₑ, α₅, BD, ϕ₁₀₀, PD, Dstp, P₀, b = params
-		C = FT(2900.0)*(z+FT(5))/FT(5) + 100 
+		C = FT(2900.0)*(z+FT(1))/FT(1) + 100 
 		return FT(C)
 	end
 	Y.DETECT.C .= CO2_profile.(z, Ref(params))
