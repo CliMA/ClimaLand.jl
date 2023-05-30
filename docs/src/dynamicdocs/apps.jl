6f7a49a69b34b4d25e136d6f04c2b7f66dae897f
AlexisRenchon <a.renchon@gmail.com>
2023-05-30 12:37:01 -0700

    little fix

--- docs/src/dynamicdocs/apps.jl
+++ docs/src/dynamicdocs/apps.jl
@@ -11,7 +11,8 @@ port = 9385
 server = Server(IP, port; proxy_url="https://clima.westus3.cloudapp.azure.com/jsserve/")
 
 # Load and create apps
-include("apps/leaf_An.jl"); An_app = An_app_f()
+include("leaf_An.jl"); An_app = An_app_f()
+An_app = An_app_f() # need to run twice for unicode character... (bug)
 
 # Route apps
 route!(server,"/leaf_An"=>An_app)
