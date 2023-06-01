529229c5f697035a185619ae7715a2293647272a
AlexisRenchon <a.renchon@gmail.com>
2023-06-01 11:52:20 -0700

    typo

--- docs/list_dynamicdocs.jl
+++ docs/list_dynamicdocs.jl
@@ -1,7 +1,7 @@
 dynamicdocs = [
   "Vegetation" => [
     "Radiative transfer" => "dynamicdocs/pages/vegetation/radiative_transfer.md",
-    "Stomatal conductance" => "dynamicdocs/pages/vegetation/stomatal_condutance.md",
+    "Stomatal conductance" => "dynamicdocs/pages/vegetation/stomatal_conductance.md",
     "Leaf photosynthesis" => "dynamicdocs/pages/vegetation/leaf_photosynthesis.md",
                   ]
   "Soil biogeochemistry" => [
