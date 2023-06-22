27467612bf13eefef9a81f1829e008b0d6b5387e
vesperaluo <135674014+vesperaluo@users.noreply.github.com>
2023-06-22 11:41:20 -0700

    Update list_dynamicdocs.jl

--- docs/list_dynamicdocs.jl
+++ docs/list_dynamicdocs.jl
@@ -3,6 +3,7 @@ dynamicdocs = [
     "Radiative transfer" => "dynamicdocs/pages/vegetation/radiative_transfer.md",
     "Stomatal conductance" => "dynamicdocs/pages/vegetation/stomatal_conductance.md",
     "Leaf photosynthesis" => "dynamicdocs/pages/vegetation/leaf_photosynthesis.md",
+    "Farquhar Model" => "dynamicdocs/pages/vegetation/FarquharModel.md"
                   ]
   "Soil biogeochemistry" => [
     "Microbial respiration" => "dynamicdocs/pages/soil_biogeochemistry/microbial_respiration.md",
