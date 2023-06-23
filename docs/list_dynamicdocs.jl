f762b52a9fcb85d654205f32c407347c34bace52
vesperaluo <135674014+vesperaluo@users.noreply.github.com>
2023-06-23 10:46:27 -0700

    Update list_dynamicdocs.jl
    make the sub-menus consistent

--- docs/list_dynamicdocs.jl
+++ docs/list_dynamicdocs.jl
@@ -1,9 +1,19 @@
 dynamicdocs = [
   "Vegetation" => [
-    "Radiative transfer" => "dynamicdocs/pages/vegetation/radiative_transfer.md",
-    "Stomatal conductance" => "dynamicdocs/pages/vegetation/stomatal_conductance.md",
     "Leaf photosynthesis" => "dynamicdocs/pages/vegetation/leaf_photosynthesis.md",
-    "Farquhar Model" => "dynamicdocs/pages/vegetation/FarquharModel.md"
+    "Radiative transfer" => [
+      "Beer model" => "dynamicdocs/pages/vegetation/beer_model.md"
+                            ]
+    "Photosynthesis" => [
+      "Farquhar model" => "dynamicdocs/pages/vegetation/farquhar_model.md"
+      "Optimality model" => "dynamicdocs/pages/vegetation/optimality_model.md"
+                        ]
+    "Stomatal conductance" => [
+      "Medlyn model" => "dynamicdocs/pages/vegetation/medlyn_model.md"
+                              ]
+    "Plant hydraulics" => [
+      "Van Genuchten model" => "dynamicdocs/pages/vegetation/van_genuchten_model.md"
+                          ]
                   ]
   "Soil biogeochemistry" => [
     "Microbial respiration" => "dynamicdocs/pages/soil_biogeochemistry/microbial_respiration.md",
