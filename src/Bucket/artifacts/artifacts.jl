8dda1d9c43c72b06f4f627fab09be37fe2df9f56
Julia Sloan <jsloan@caltech.edu>
2023-07-06 13:33:10 -0700

    remove extra albedo dataset

--- src/Bucket/artifacts/artifacts.jl
+++ src/Bucket/artifacts/artifacts.jl
@@ -30,36 +30,6 @@ function cesm2_albedo_dataset_path()
     )
 end
 
-# https://caltech.box.com/shared/static/zx07ipsdkv8cbx9wmk4v24e49jkcyefq.nc
-"""
-    cesm2_land_albedo_dataset_path()
-
-Triggers the download of the CESM2 LS3MIP land albedo dataset, if not
-already downloaded, using Julia Artifacts, and returns the path to
-this file.
-
-This file contains surface albedo over land. NaN values have been replaced
-by 1, since we expect that regions having 0 incoming radiation are
-experiencing winter storms and therefore likely have high albedo at the
-surface (snow, ice, etc). It was created by Renato Braghiere by
-calculating outgoing SW radiation / incoming SW radiation at the surface
-using CESM2 data.
-"""
-function cesm2_land_albedo_dataset_path()
-    land_albedo_dataset = AW.ArtifactWrapper(
-        @__DIR__,
-        "land_albedo", # TODO change varname?
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/zx07ipsdkv8cbx9wmk4v24e49jkcyefq.nc",
-            filename = "rsalb_Amon_CESM2_land-hist_r1i1p1f1_gn_185001-201512.nc",
-        ),],
-    )
-    path = AW.get_data_folder(land_albedo_dataset)
-    return joinpath(
-        path,
-        "rsalb_Amon_CESM2_land-hist_r1i1p1f1_gn_185001-201512.nc",
-    )
-end
 """
     bareground_albedo_dataset_path()
 
