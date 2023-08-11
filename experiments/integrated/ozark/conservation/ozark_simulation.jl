262f7050080c871aee8a74bd8d5140ac522d2f62
kmdeck <kdeck@caltech.edu>
2023-08-11 11:23:11 -0700

    plot water conservation
    water conservation experiment
    
    reduce diff
    
    documented script better

---
+++ experiments/integrated/ozark/conservation/ozark_simulation.jl
@@ -0,0 +1,19 @@
+t0 = FT(120 * 3600 * 24)# start mid year
+N_days = 10
+tf = t0 + FT(3600 * 24 * N_days)
+dt = FT(300)
+n = 1
+saveat = Array(t0:(n * dt):tf)
+timestepper = CTS.ARS111()
+norm_condition = CTS.MaximumError(FT(1e-8))
+conv_checker = CTS.ConvergenceChecker(; norm_condition)
+max_iterations = 20
+# Set up timestepper
+ode_algo = CTS.IMEXAlgorithm(
+    timestepper,
+    CTS.NewtonsMethod(
+        max_iters = max_iterations,
+        update_j = CTS.UpdateEvery(CTS.NewNewtonIteration),
+        convergence_checker = conv_checker,
+    ),
+)
