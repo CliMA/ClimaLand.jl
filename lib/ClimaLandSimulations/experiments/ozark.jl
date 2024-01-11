b06b184971f7086a681effd9468a9d661eba58d4
Alexis Renchon <a.renchon@gmail.com>
2024-01-11 10:02:11 -0800

    1st commit

--- lib/ClimaLandSimulations/experiments/ozark.jl
+++ lib/ClimaLandSimulations/experiments/ozark.jl
@@ -4,4 +4,5 @@ using ClimaLandSimulations
 sv, sol, Y, p, inputs, climalsm = fluxnet_simulation("US-MOz");
 
 # Will save figures in current repo /figures
-make_plots(inputs, climalsm)
+make_plots(inputs, climalsm) 
+
