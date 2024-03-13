6ff0ed01405f5c514fe311797fe874807e2e0b34
Gabriele Bozzola <sbozzolator@gmail.com>
2024-03-13 09:44:57 -0700

    Add benchmark for global bucket

--- experiments/standalone/Bucket/global_bucket_temporalmap.jl
+++ experiments/standalone/Bucket/global_bucket_temporalmap.jl
@@ -38,6 +38,14 @@ using ClimaLand:
     PrescribedRadiativeFluxes
 using ClimaLand.TimeVaryingInputs
 
+PROFILING = false
+try
+    import Profile, ProfileCanvas
+    global PROFILING = true
+    @info "ProfileCanvas found, running with profiler"
+catch
+end
+
 """
    compute_extrema(v)
 
@@ -60,33 +68,7 @@ outdir = joinpath(
     "experiments/standalone/Bucket/artifacts_temporalmap",
 )
 !ispath(outdir) && mkpath(outdir)
-
-# Set up simulation domain
-soil_depth = FT(3.5);
-bucket_domain = ClimaLand.Domains.SphericalShell(;
-    radius = FT(6.3781e6),
-    depth = soil_depth,
-    nelements = (10, 10),
-    npolynomial = 1,
-    dz_tuple = FT.((1.0, 0.05)),
-);
-ref_time = DateTime(2005);
-
-# Initialize parameters
-σS_c = FT(0.2);
-W_f = FT(0.15);
-z_0m = FT(1e-2);
-z_0b = FT(1e-3);
-κ_soil = FT(0.7);
-ρc_soil = FT(2e6);
-τc = FT(3600);
-t0 = 0.0;
-# run for 50 days to test monthly file update
-tf = 50 * 86400;
-Δt = 3600.0;
-
-# Construct albedo parameter object using temporal map
-# Use separate regridding directory for CPU and GPU runs to avoid race condition
+# Use separate output directory for CPU and GPU runs to avoid race condition
 device_suffix =
     typeof(ClimaComms.context().device) <: ClimaComms.CPUSingleThreaded ?
     "cpu" : "gpu"
@@ -95,88 +77,157 @@ regrid_dir = joinpath(
     "experiments/standalone/Bucket/$device_suffix/regrid-temporal/",
 )
 !ispath(regrid_dir) && mkpath(regrid_dir)
-surface_space = bucket_domain.space.surface
-albedo = PrescribedSurfaceAlbedo{FT}(regrid_dir, ref_time, t0, surface_space);
+t0 = 0.0;
+# run for 50 days to test monthly file update
+tf = 50 * 86400;
+Δt = 3600.0;
 
-bucket_parameters = BucketModelParameters(FT; albedo, z_0m, z_0b, τc);
 
-# Precipitation:
-precip = (t) -> 0;
-snow_precip = (t) -> -5e-7 * (t < 1 * 86400);
-# Diurnal temperature variations:
-T_atmos = (t) -> 275.0 + 5.0 * sin(2.0 * π * t / 86400 - π / 2);
-# Constant otherwise:
-u_atmos = (t) -> 3.0;
-q_atmos = (t) -> 0.001;
-h_atmos = FT(2);
-P_atmos = (t) -> 101325;
+function setup_prob(t0, tf, Δt)
+    # We set up the problem in a function so that we can make multiple copies (for profiling)
 
-bucket_atmos = PrescribedAtmosphere(
-    TimeVaryingInput(precip),
-    TimeVaryingInput(snow_precip),
-    TimeVaryingInput(T_atmos),
-    TimeVaryingInput(u_atmos),
-    TimeVaryingInput(q_atmos),
-    TimeVaryingInput(P_atmos),
-    ref_time,
-    h_atmos,
-);
+    # Set up simulation domain
+    soil_depth = FT(3.5)
+    bucket_domain = ClimaLand.Domains.SphericalShell(;
+        radius = FT(6.3781e6),
+        depth = soil_depth,
+        nelements = (10, 10),
+        npolynomial = 1,
+        dz_tuple = FT.((1.0, 0.05)),
+    )
+    ref_time = DateTime(2005)
 
-# Prescribed radiation -- a prescribed downwelling SW diurnal cycle, with a
-# peak at local noon, and a prescribed downwelling LW radiative
-# flux, assuming the air temperature is on average 275 degrees
-# K with a diurnal amplitude of 5 degrees K:
-SW_d = (t) -> max(1361 * sin(2π * t / 86400 - π / 2), 0.0);
-LW_d = (t) -> 5.67e-8 * (275.0 + 5.0 * sin(2.0 * π * t / 86400 - π / 2))^4;
-bucket_rad = PrescribedRadiativeFluxes(
-    FT,
-    TimeVaryingInput(SW_d),
-    TimeVaryingInput(LW_d),
-    ref_time,
-);
+    # Initialize parameters
+    σS_c = FT(0.2)
+    W_f = FT(0.15)
+    z_0m = FT(1e-2)
+    z_0b = FT(1e-3)
+    κ_soil = FT(0.7)
+    ρc_soil = FT(2e6)
+    τc = FT(3600)
 
-model = BucketModel(
-    parameters = bucket_parameters,
-    domain = bucket_domain,
-    atmosphere = bucket_atmos,
-    radiation = bucket_rad,
-);
+    surface_space = bucket_domain.space.surface
+    # Construct albedo parameter object using temporal map
+    albedo =
+        PrescribedSurfaceAlbedo{FT}(regrid_dir, ref_time, t0, surface_space)
 
-Y, p, coords = initialize(model);
+    bucket_parameters = BucketModelParameters(FT; albedo, z_0m, z_0b, τc)
 
-Y.bucket.T .= FT(270);
-Y.bucket.W .= FT(0.05);
-Y.bucket.Ws .= FT(0.0);
-Y.bucket.σS .= FT(0.08);
+    # Precipitation:
+    precip = (t) -> 0
+    snow_precip = (t) -> -5e-7 * (t < 1 * 86400)
+    # Diurnal temperature variations:
+    T_atmos = (t) -> 275.0 + 5.0 * sin(2.0 * π * t / 86400 - π / 2)
+    # Constant otherwise:
+    u_atmos = (t) -> 3.0
+    q_atmos = (t) -> 0.001
+    h_atmos = FT(2)
+    P_atmos = (t) -> 101325
 
-set_initial_cache! = make_set_initial_cache(model);
-set_initial_cache!(p, Y, t0);
-exp_tendency! = make_exp_tendency(model);
+    bucket_atmos = PrescribedAtmosphere(
+        TimeVaryingInput(precip),
+        TimeVaryingInput(snow_precip),
+        TimeVaryingInput(T_atmos),
+        TimeVaryingInput(u_atmos),
+        TimeVaryingInput(q_atmos),
+        TimeVaryingInput(P_atmos),
+        ref_time,
+        h_atmos,
+    )
+
+    # Prescribed radiation -- a prescribed downwelling SW diurnal cycle, with a
+    # peak at local noon, and a prescribed downwelling LW radiative
+    # flux, assuming the air temperature is on average 275 degrees
+    # K with a diurnal amplitude of 5 degrees K:
+    SW_d = (t) -> max(1361 * sin(2π * t / 86400 - π / 2), 0.0)
+    LW_d = (t) -> 5.67e-8 * (275.0 + 5.0 * sin(2.0 * π * t / 86400 - π / 2))^4
+    bucket_rad = PrescribedRadiativeFluxes(
+        FT,
+        TimeVaryingInput(SW_d),
+        TimeVaryingInput(LW_d),
+        ref_time,
+    )
+
+
+    model = BucketModel(
+        parameters = bucket_parameters,
+        domain = bucket_domain,
+        atmosphere = bucket_atmos,
+        radiation = bucket_rad,
+    )
+
+    Y, p, _coords = initialize(model)
+
+    Y.bucket.T .= FT(270)
+    Y.bucket.W .= FT(0.05)
+    Y.bucket.Ws .= FT(0.0)
+    Y.bucket.σS .= FT(0.08)
+
+    set_initial_cache! = make_set_initial_cache(model)
+    set_initial_cache!(p, Y, t0)
+    exp_tendency! = make_exp_tendency(model)
+    prob = SciMLBase.ODEProblem(
+        CTS.ClimaODEFunction((T_exp!) = exp_tendency!, (dss!) = ClimaLand.dss!),
+        Y,
+        (t0, tf),
+        p,
+    )
+    saveat = collect(t0:(Δt * 3):tf)
+    saved_values = (;
+        t = Array{Float64}(undef, length(saveat)),
+        saveval = Array{NamedTuple}(undef, length(saveat)),
+    )
+    saving_cb = ClimaLand.NonInterpSavingCallback(saved_values, saveat)
+    updateat = copy(saveat)
+    updatefunc = ClimaLand.make_update_drivers(bucket_atmos, bucket_rad)
+    driver_cb = ClimaLand.DriverUpdateCallback(updateat, updatefunc)
+    cb = SciMLBase.CallbackSet(driver_cb, saving_cb)
+
+    return prob, cb, saveat, saved_values
+end
+
+prob, cb, saveat, saved_values = setup_prob(t0, tf, Δt);
 timestepper = CTS.RK4()
 ode_algo = CTS.ExplicitAlgorithm(timestepper)
-prob = SciMLBase.ODEProblem(
-    CTS.ClimaODEFunction(T_exp! = exp_tendency!, dss! = ClimaLand.dss!),
-    Y,
-    (t0, tf),
-    p,
-);
-
-saveat = collect(t0:(Δt * 3):tf);
-saved_values = (;
-    t = Array{Float64}(undef, length(saveat)),
-    saveval = Array{NamedTuple}(undef, length(saveat)),
-);
-saving_cb = ClimaLand.NonInterpSavingCallback(saved_values, saveat);
-updateat = copy(saveat)
-updatefunc = ClimaLand.make_update_drivers(bucket_atmos, bucket_rad)
-driver_cb = ClimaLand.DriverUpdateCallback(updateat, updatefunc)
-cb = SciMLBase.CallbackSet(driver_cb, saving_cb)
 
 @time sol =
-    SciMLBase.solve(prob, ode_algo; dt = Δt, saveat = saveat, callback = cb);
+    SciMLBase.solve(prob, ode_algo; dt = Δt, saveat = saveat, callback = cb)
+if PROFILING
+    # Now that we compiled, solve again but collect profiling information
+
+    # We run only for one day for profiling
+    tf = 86400.0
+    prob, cb, saveat, _ = setup_prob(t0, tf, Δt)
+
+    Profile.@profile SciMLBase.solve(
+        prob,
+        ode_algo;
+        dt = Δt,
+        saveat = saveat,
+        callback = cb,
+    )
+    results = Profile.fetch()
+    flame_file = joinpath(outdir, "flame_$device_suffix.html")
+    ProfileCanvas.html_file(flame_file, results)
+    @info "Save compute flame to $flame_file"
+
+    prob, cb, saveat, _ = setup_prob(t0, tf, Δt)
+    Profile.Allocs.@profile sample_rate = 1.0 SciMLBase.solve(
+        prob,
+        ode_algo;
+        dt = Δt,
+        saveat = saveat,
+        callback = cb,
+    )
+    results = Profile.Allocs.fetch()
+    profile = ProfileCanvas.view_allocs(results)
+    alloc_flame_file = joinpath(outdir, "alloc_flame_$device_suffix.html")
+    ProfileCanvas.html_file(alloc_flame_file, profile)
+    @info "Save allocation flame to $alloc_flame_file"
+end
 
 # Interpolate to grid
-space = axes(coords.surface)
+space = axes(sol.prob.p.drivers.T)
 longpts = range(-180.0, 180.0, 21)
 latpts = range(-90.0, 90.0, 21)
 hcoords = [Geometry.LatLongPoint(lat, long) for long in longpts, lat in latpts]
