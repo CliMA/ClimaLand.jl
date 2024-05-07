336b619c7da6f19239c182c6c028cac49c787690
Alexis Renchon <a.renchon@gmail.com>
2024-05-07 11:27:37 -0700

    1st commit

---
+++ src/Diagnostics/Diagnostics.jl
@@ -0,0 +1,17 @@
+module Diagnostics
+
+import ClimaDiagnostics:
+    DiagnosticVariable,
+    ScheduledDiagnostic,
+    average_pre_output_hook!,
+    DiagnosticsCallback
+
+import ClimaDiagnostics.DiagnosticVariables: descriptive_short_name
+
+import ClimaDiagnostics.Schedules: EveryStepSchedule, EveryDtSchedule
+
+import ClimaDiagnostics.Writers: HDF5Writer, NetCDFWriter, write_field!
+
+include("diagnostic.jl")
+
+end
