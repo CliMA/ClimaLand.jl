b6e5b2eeceaeba2fae7bfea934b01e659116b082
Gabriele Bozzola <sbozzolator@gmail.com>
2024-06-03 17:23:05 -0700

    This is crazy

--- test/standalone/Soil/soil_test_3d.jl
+++ test/standalone/Soil/soil_test_3d.jl
@@ -1,16 +1,35 @@
-using Test
-import ClimaComms
-@static pkgversion(ClimaComms) >= v"0.6" && ClimaComms.@import_required_backends
-using Statistics
-using ClimaCore
-import ClimaParams as CP
-using ClimaLand
-using ClimaLand.Domains: HybridBox, SphericalShell
-using ClimaLand.Soil
-import ClimaLand
-import ClimaLand.Parameters as LP
-
 FT = Float64
-@show x = 10 ^ 11 * eps(FT)
+using Test
+x = (10 ^ 11) * eps(FT)
 # x - 2eps() ≤ x ≤ x + 2eps()
-@test 2.220446049205904e-5 ≤ x ≤ 2.220446049294722e-5
+# @test 2.220446049205904e-5 ≤ x ≤ 2.220446049294722e-5
+
+@show eps(FT)
+@show (10 ^ 1) * eps(FT)
+@show (10 ^ 2) * eps(FT)
+@show (10 ^ 3) * eps(FT)
+@show (10 ^ 4) * eps(FT)
+@show (10 ^ 5) * eps(FT)
+@show (10 ^ 6) * eps(FT)
+@show (10 ^ 7) * eps(FT)
+@show (10 ^ 8) * eps(FT)
+@show (10 ^ 9) * eps(FT)
+@show (10 ^ 10) * eps(FT)
+@show (10 ^ 11) * eps(FT)
+
+x = (10 ^ 11) * eps(Float64)
+# x - 2eps() ≤ x ≤ x + 2eps()
+# @test 2.220446049205904e-5 ≤ x ≤ 2.220446049294722e-5
+
+@show eps(Float64)
+@show (10 ^ 1) * eps(Float64)
+@show (10 ^ 2) * eps(Float64)
+@show (10 ^ 3) * eps(Float64)
+@show (10 ^ 4) * eps(Float64)
+@show (10 ^ 5) * eps(Float64)
+@show (10 ^ 6) * eps(Float64)
+@show (10 ^ 7) * eps(Float64)
+@show (10 ^ 8) * eps(Float64)
+@show (10 ^ 9) * eps(Float64)
+@show (10 ^ 10) * eps(Float64)
+@show (10 ^ 11) * eps(Float64)
