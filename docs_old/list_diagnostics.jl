20d2ca1765c0008ce02f2f1795d67450637133c8
AlexisRenchon <a.renchon@gmail.com>
2024-09-02 10:32:43 -0700

    do not track old docs

--- docs_old/list_diagnostics.jl
+++
@@ -1,5 +0,0 @@
-diagnostics = [
-    "For users" => "diagnostics/users_diagnostics.md",
-    "For developers" => "diagnostics/developers_diagnostics.md",
-    "Available diagnostics" => "diagnostics/available_diagnostics.md",
-]
