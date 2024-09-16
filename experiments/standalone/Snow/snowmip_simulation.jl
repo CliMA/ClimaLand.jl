1164307e100170780c7f13123def4ab54f7793ac
kmdeck <kdeck@caltech.edu>
2024-09-16 14:55:32 -0700

    conservation, change of prognostic variable

--- experiments/standalone/Snow/snowmip_simulation.jl
+++ experiments/standalone/Snow/snowmip_simulation.jl
@@ -1,6 +1,6 @@
 import SciMLBase
 import ClimaTimeSteppers as CTS
-using Plots
+using CairoMakie
 import ClimaParams as CP
 import ClimaUtilities.TimeVaryingInputs: TimeVaryingInput
 import ClimaUtilities.ClimaArtifacts: @clima_artifact
@@ -43,7 +43,7 @@ tf = FT(seconds[end])
 ndays = (tf - t0) / 3600 / 24
 Δt = FT(60 * 60)
 
-domain = Point(; z_sfc = FT(0))
+domain = ClimaLand.Domains.Point(; z_sfc = FT(0))
 
 parameters =
     SnowParameters{FT}(Δt; α_snow = α, ρ_snow = ρ, earth_param_set = param_set)
@@ -63,16 +63,28 @@ Y.snow.U .=
 set_initial_cache! = ClimaLand.make_set_initial_cache(model)
 set_initial_cache!(p, Y, t0)
 exp_tendency! = ClimaLand.make_exp_tendency(model)
-timestepper = CTS.RK4()
-ode_algo = CTS.ExplicitAlgorithm(timestepper)
+# We use ARS111 (equivalent to explicit Euler) here for ease of assessing conservation
+timestepper = CTS.ARS111()
+ode_algo = CTS.IMEXAlgorithm(
+    timestepper,
+    CTS.NewtonsMethod(
+        max_iters = 1,
+        update_j = CTS.UpdateEvery(CTS.NewTimeStep),
+    ),
+);
+
 
 prob = SciMLBase.ODEProblem(
-    CTS.ClimaODEFunction(T_exp! = exp_tendency!, dss! = ClimaLand.dss!),
+    CTS.ClimaODEFunction(
+        T_exp! = exp_tendency!,
+        T_imp! = nothing,
+        dss! = ClimaLand.dss!,
+    ),
     Y,
     (t0, tf),
     p,
 )
-saveat = FT.(collect(t0:3600:tf))
+saveat = FT.(collect(t0:Δt:tf))
 sv = (;
     t = Array{FT}(undef, length(saveat)),
     saveval = Array{NamedTuple}(undef, length(saveat)),
@@ -105,7 +117,8 @@ water_runoff =
     [parent(sv.saveval[k].snow.water_runoff)[1] for k in 1:length(sol.t)];
 rain = [parent(sv.saveval[k].drivers.P_liq)[1] for k in 1:length(sv.t)];
 snow = [parent(sv.saveval[k].drivers.P_snow)[1] for k in 1:length(sv.t)];
-
+scf =
+    [parent(sv.saveval[k].snow.snow_cover_fraction)[1] for k in 1:length(sol.t)];
 
 S = [parent(sol.u[k].snow.S)[1] for k in 1:length(sol.t)];
 U = [parent(sol.u[k].snow.U)[1] for k in 1:length(sol.t)];
@@ -137,118 +150,190 @@ mean_obs_df = combine(
     [:model_swe, :obs_swe, :model_tsnow, :obs_tsnow] .=> missingmean,
     renamecols = false,
 )
+daily = t ./ 24 ./ 3600
 
-plot1a = plot()
-plot!(
-    plot1a,
-    t ./ 3600 ./ 24,
-    S,
-    label = "Model",
-    legend = :topright,
-    xticks = :none,
-    ylabel = "Snow water",
-    xlim = (0, ndays),
-)
-scatter!(plot1a, seconds[snow_data_avail] ./ 24 ./ 3600, SWE, label = "Data")
+fig = CairoMakie.Figure(size = (1600, 1200), fontsize = 26)
+# set limits
+ax1 = CairoMakie.Axis(fig[1, 1], ylabel = "Water Vol/Area ground")
 
-plot1b = plot(xlim = (0, ndays))
-plot!(
-    plot1b,
-    t ./ 3600 ./ 24,
-    q_l,
-    xlabel = "Time (days)",
-    label = "",
-    ylabel = "Liquid mass fraction",
-)
-
-plot1c = plot()
-plot!(
-    plot1c,
-    mean_obs_df.doy,
-    mean_obs_df.model_swe,
-    label = "Model",
-    xlabel = "Day of year",
-    ylabel = "Average snow water",
-)
-scatter!(plot1c, mean_obs_df.doy, mean_obs_df.obs_swe, label = "Data")
-
-plot(plot1a, plot1b, plot1c, layout = (3, 1), size = (800, 800))
-savefig(joinpath(savedir, "snow_water_content_$(SITE_NAME).png"))
-
-plot2a = plot(xlim = (0, ndays), ylim = (250, 280))
-plot!(
-    plot2a,
-    t ./ 3600 ./ 24,
-    T,
-    label = "Model",
-    legend = :bottomright,
-    xticks = :none,
-    ylabel = "Snow Temperature",
-)
-scatter!(
-    plot2a,
-    seconds[snow_data_avail] ./ 3600 ./ 24,
-    T_snow .+ 273.15,
+xlims!(ax1, 0, ndays)
+CairoMakie.hidexdecorations!(ax1, ticks = false)
+CairoMakie.lines!(ax1, daily, S, label = "Model")
+CairoMakie.scatter!(
+    ax1,
+    seconds[snow_data_avail] ./ 24 ./ 3600,
+    SWE,
     label = "Data",
+    color = :red,
 )
-# Plot the observed atmospheric temperature
-scatter!(
-    plot2a,
-    seconds[snow_data_avail] ./ 3600 ./ 24,
+CairoMakie.axislegend(ax1, position = :rt)
+
+ax2 = CairoMakie.Axis(
+    fig[2, 1],
+    ylabel = "Liquid Mass Fraction",
+    xlabel = "Time of year (days)",
+)
+CairoMakie.lines!(ax2, daily, q_l, label = "")
+xlims!(ax2, 0, ndays)
+ax3 = CairoMakie.Axis(
+    fig[3, 1],
+    ylabel = "Average Snow Water",
+    xlabel = "Day of Year",
+)
+CairoMakie.lines!(ax3, mean_obs_df.doy, mean_obs_df.model_swe, label = "Model")
+CairoMakie.scatter!(
+    ax3,
+    mean_obs_df.doy,
+    mean_obs_df.obs_swe,
+    label = "Data",
+    color = :red,
+)
+CairoMakie.save(joinpath(savedir, "snow_water_content_$(SITE_NAME).png"), fig)
+
+
+
+
+fig = CairoMakie.Figure(size = (1600, 1200), fontsize = 26)
+# set limits
+ax1 = CairoMakie.Axis(fig[1, 1], ylabel = "Temperature")
+
+xlims!(ax1, 0, ndays)
+CairoMakie.hidexdecorations!(ax1, ticks = false)
+CairoMakie.lines!(ax1, daily, T, label = "Model")
+CairoMakie.scatter!(
+    ax1,
+    seconds[snow_data_avail] ./ 24 ./ 3600,
+    T_snow .+ 273.15,
+    label = "Snow T",
+    color = :red,
+)
+CairoMakie.scatter!(
+    ax1,
+    seconds[snow_data_avail] ./ 24 ./ 3600,
     Tair[snow_data_avail],
-    label = "Atmospheric Temperature",
+    label = "Atmosphere T",
+    color = :orange,
 )
+CairoMakie.axislegend(ax1, position = :rt)
 
-plot2b = plot(xlim = (0, ndays))
-plot!(
-    plot2b,
-    t ./ 3600 ./ 24,
-    U,
-    label = "",
-    xlabel = "Time (days)",
-    ylabel = "Snow Energy per Area",
+ax2 = CairoMakie.Axis(
+    fig[2, 1],
+    ylabel = "Snow Energy/Ground Area",
+    xlabel = "Time of year (days)",
 )
-
-plot2c = plot()
-plot!(
-    plot2c,
+CairoMakie.lines!(ax2, daily, U, label = "")
+xlims!(ax2, 0, ndays)
+ax3 = CairoMakie.Axis(
+    fig[3, 1],
+    ylabel = "Average Snow Temperature",
+    xlabel = "Day of Year",
+)
+CairoMakie.lines!(
+    ax3,
     mean_obs_df.doy,
     mean_obs_df.model_tsnow,
     label = "Model",
-    xlabel = "Day of year",
-    ylabel = "Average snow temperature",
 )
-scatter!(plot2c, mean_obs_df.doy, mean_obs_df.obs_tsnow, label = "Data")
+CairoMakie.scatter!(
+    ax3,
+    mean_obs_df.doy,
+    mean_obs_df.obs_tsnow,
+    label = "Data",
+    color = :red,
+)
+CairoMakie.save(joinpath(savedir, "snow_energy_content_$(SITE_NAME).png"), fig)
 
-snow_energy_plot =
-    plot(plot2a, plot2b, plot2c, layout = (3, 1), size = (800, 800))
-savefig(joinpath(savedir, "snow_energy_content_$(SITE_NAME).png"))
 
-plot3 = plot(
-    xlabel = "Time (days)",
+
+fig = CairoMakie.Figure(size = (1000, 1000), fontsize = 26)
+# set limits
+ax1 = CairoMakie.Axis(
+    fig[1, 1],
     ylabel = "Cumulative height (m)",
-    xlim = (0, ndays),
+    xlabel = "Time(days)",
 )
-plot!(plot3, t ./ 3600 ./ 24, cumsum(snow) .* Δt, label = "Snow", color = "red")
-plot!(
-    plot3,
-    t ./ 3600 ./ 24,
+xlims!(ax1, 0, ndays)
+CairoMakie.lines!(ax1, daily, cumsum(snow) .* Δt, label = "Snow", color = :red)
+CairoMakie.lines!(
+    ax1,
+    daily,
     cumsum(rain) .* Δt,
     label = "Rain",
-    color = "green",
+    color = :green,
 )
-plot!(
-    plot3,
-    t ./ 3600 ./ 24,
-    cumsum(evaporation) .* Δt,
+CairoMakie.lines!(
+    ax1,
+    daily,
+    cumsum(evaporation .* scf) .* Δt,
     label = "Evaporation",
-    color = "purple",
+    color = :purple,
 )
-plot!(
-    plot3,
-    t ./ 3600 ./ 24,
-    cumsum(water_runoff) .* Δt,
-    label = "Runoff",
-    color = "blue",
+CairoMakie.lines!(
+    ax1,
+    daily,
+    cumsum(water_runoff .* scf) .* Δt,
+    label = "Runoff/Melt",
+    color = :blue,
 )
-savefig(joinpath(savedir, "water_fluxes_$(SITE_NAME).png"))
+CairoMakie.axislegend(ax1, position = :lb)
+
+CairoMakie.save(joinpath(savedir, "water_fluxes_$(SITE_NAME).png"), fig)
+
+# Assess conservation
+fig = CairoMakie.Figure(size = (1600, 1200), fontsize = 26)
+ax1 = CairoMakie.Axis(fig[2, 1], ylabel = "ΔEnergy (J/A)", xlabel = "Days")
+ΔE_expected =
+    cumsum(
+        -1 .* [
+            parent(sv.saveval[k].snow.applied_energy_flux)[end] for
+            k in 2:1:length(sv.t)
+        ],
+    ) * (sv.t[2] - sv.t[1])
+E_measured = [parent(sol.u[k].snow.U)[end] for k in 1:1:length(sv.t)]
+ΔW_expected =
+    cumsum(
+        -1 .* [
+            parent(sv.saveval[k].snow.applied_water_flux)[end] for
+            k in 2:1:length(sv.t)
+        ],
+    ) * (sv.t[2] - sv.t[1])
+W_measured = [parent(sol.u[k].snow.S)[end] for k in 1:1:length(sv.t)]
+CairoMakie.lines!(
+    ax1,
+    daily[2:end],
+    E_measured[2:end] .- E_measured[1],
+    label = "Simulated",
+)
+CairoMakie.lines!(ax1, daily[2:end], ΔE_expected, label = "Expected")
+CairoMakie.axislegend(ax1, position = :rt)
+
+# Temp
+ax4 = CairoMakie.Axis(fig[1, 1], ylabel = "ΔWater (m)")
+CairoMakie.hidexdecorations!(ax4, ticks = false)
+CairoMakie.lines!(
+    ax4,
+    daily[2:end],
+    W_measured[2:end] .- W_measured[1],
+    label = "Simulated",
+)
+
+CairoMakie.lines!(ax4, daily[2:end], ΔW_expected, label = "Expected")
+CairoMakie.axislegend(ax4, position = :rt)
+
+
+ax3 = CairoMakie.Axis(fig[2, 2], ylabel = "ΔE/E", xlabel = "Days")
+CairoMakie.lines!(
+    ax3,
+    daily[2:end],
+    (E_measured[2:end] .- E_measured[1] .- ΔE_expected) ./ mean(E_measured),
+)
+
+ax2 = CairoMakie.Axis(fig[1, 2], ylabel = "ΔW/W")
+CairoMakie.hidexdecorations!(ax2, ticks = false)
+CairoMakie.lines!(
+    ax2,
+    daily[2:end],
+    (W_measured[2:end] .- W_measured[1] .- ΔW_expected) ./ mean(W_measured),
+)
+
+CairoMakie.save(joinpath(savedir, "conservation_$(SITE_NAME).png"), fig)
