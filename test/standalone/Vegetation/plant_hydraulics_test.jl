dbaa724c4d8d6c7a1a4eba66f43fd017c0be5eb6
Teja Reddy <treddy0205@gmail.com>
2024-10-07 13:31:10 -0700

    Fix plant_hydraulics_test to work on GPU (#813)
    * Revert plant_hydraulics_test to use scalar root_depth
    
    When root_distribution was removed, it was replaced with
    rooting_depth, which can be a field. The Plant hydraulics model integration tests
    was changed to test rooting_depth as both a scalar and a field. In order to
    make the broadcast work, a wrapper function was used around NLsolve. This works
    fine with cpu, but errors on gpu because NLSolve does not return a bitstype.
    This commit reverts the test implementation and only tests rooting_depth as
    a field.
    
    * Change Plant hydraulics model integration tests to use fields
    
    Plant hydraulics model integration tests now tests rooting_depth
    as a field and as a float. The issue with this test and fields on the GPU
    was the usage of NLSolve. This test first solves for the steady state of the hydraulics
    system using NLSolve. Then, the solution is used to check that evaluating the
    tendecy of the model also results in a steady state. Because the rooting_depth
    field is constant, this commit uses NLsolve with a float of that constant.
    Then, when the model's tendency is evaluated, the field is used.

--- test/standalone/Vegetation/plant_hydraulics_test.jl
+++ test/standalone/Vegetation/plant_hydraulics_test.jl
@@ -238,6 +238,7 @@ for FT in (Float32, Float64)
             range(start = 0.0, step = Δz, stop = Δz * (n_stem + n_leaf)),
         )
 
+
         function leaf_transpiration(t)
             T = FT(1e-8) # m/s
         end
@@ -246,45 +247,44 @@ for FT in (Float32, Float64)
         transpiration = PrescribedTranspiration{FT}(leaf_transpiration)
 
         soil_driver = PrescribedSoil(FT)
-
         autotrophic_parameters = AutotrophicRespirationParameters(FT)
         autotrophic_respiration_model =
             AutotrophicRespirationModel{FT}(autotrophic_parameters)
+        RD = FT(0.5)
         for domain in domains
-            param_set = PlantHydraulics.PlantHydraulicsParameters(;
-                ai_parameterization = ai_parameterization,
-                ν = plant_ν,
-                S_s = plant_S_s,
-                rooting_depth = fill(FT(0.5), domain.space.surface),
-                conductivity_model = conductivity_model,
-                retention_model = retention_model,
-            )
-
-            plant_hydraulics = PlantHydraulics.PlantHydraulicsModel{FT}(;
-                parameters = param_set,
-                transpiration = transpiration,
-                n_stem = n_stem,
-                n_leaf = n_leaf,
-                compartment_surfaces = compartment_surfaces,
-                compartment_midpoints = compartment_midpoints,
-            )
-
-            model = ClimaLand.Canopy.CanopyModel{FT}(;
-                parameters = shared_params,
-                domain = domain,
-                autotrophic_respiration = autotrophic_respiration_model,
-                radiative_transfer = rt_model,
-                photosynthesis = photosynthesis_model,
-                conductance = stomatal_model,
-                hydraulics = plant_hydraulics,
-                soil_driver = soil_driver,
-                atmos = atmos,
-                radiation = radiation,
-            )
-            # Create a system of equations for a given rooting_depth for each cell
-            function create_exp_tendency_func(rooting_depth)
-                # Solve for hydrostatic equilibrium
-                return function initial_compute_exp_tendency!(F, Y)
+            # check once with rooting_depth as float and once as field
+            for rooting_depth in (RD, fill(RD, domain.space.surface))
+                plant_hydraulics_param_set =
+                    PlantHydraulics.PlantHydraulicsParameters(;
+                        ai_parameterization = ai_parameterization,
+                        ν = plant_ν,
+                        S_s = plant_S_s,
+                        rooting_depth = rooting_depth,
+                        conductivity_model = conductivity_model,
+                        retention_model = retention_model,
+                    )
+                plant_hydraulics = PlantHydraulics.PlantHydraulicsModel{FT}(;
+                    parameters = plant_hydraulics_param_set,
+                    transpiration = transpiration,
+                    n_stem = n_stem,
+                    n_leaf = n_leaf,
+                    compartment_surfaces = compartment_surfaces,
+                    compartment_midpoints = compartment_midpoints,
+                )
+                model = ClimaLand.Canopy.CanopyModel{FT}(;
+                    parameters = shared_params,
+                    domain = domain,
+                    autotrophic_respiration = autotrophic_respiration_model,
+                    radiative_transfer = rt_model,
+                    photosynthesis = photosynthesis_model,
+                    conductance = stomatal_model,
+                    hydraulics = plant_hydraulics,
+                    soil_driver = soil_driver,
+                    atmos = atmos,
+                    radiation = radiation,
+                )
+                # Set system to hydrostatic equilibrium
+                function initial_compute_exp_tendency!(F, Y)
                     AI = (; leaf = LAI(1.0), root = RAI, stem = SAI)
                     T0A = FT(1e-8) * AI[:leaf]
                     for i in 1:(n_leaf + n_stem)
@@ -307,7 +307,7 @@ for FT in (Float32, Float64)
                                     ) .*
                                     ClimaLand.Canopy.PlantHydraulics.root_distribution.(
                                         root_depths,
-                                        rooting_depth,
+                                        RD,
                                     ) .* (
                                         vcat(root_depths, [0.0])[2:end] -
                                         vcat(root_depths, [0.0])[1:(end - 1)]
@@ -333,107 +333,81 @@ for FT in (Float32, Float64)
                         F[i] = fa - T0A
                     end
                 end
-            end
-            #=======================
-            nlsolve does not solve systems of equations that return fields, so each cell
-            must have its own system of equations that is solved for. ClimaCore fields
-            cannot hold vectors, so multiple fields must be used to hold the vector solution
-            to the system of equations. Here we solve for the steady state of the hydraulics
-            system. Then, the solution is used to check that evaluating the
-            tendecy of the model also results in a steady state. This check is repeated using
-            the plant hydraulics model directly.
-            =======================#
-            # dict to prevent recalculation when returning different index of solution
-            solutions_for_rooting_depth = Dict{FT, Vector{FT}}()
-            # solves system of equations at each cell and returns specific index of solution
-            function solve_sys_for_i(rooting_depth::FT, i) where {FT}
-                if haskey(solutions_for_rooting_depth, rooting_depth)
-                    return get(
-                        solutions_for_rooting_depth,
-                        rooting_depth,
-                        FT(i),
-                    )[i]
-                else
-                    soln = nlsolve(
-                        create_exp_tendency_func(rooting_depth),
-                        Vector{FT}(-0.03:0.01:0.07);
-                        ftol = eps(FT),
-                        method = :newton,
-                        iterations = 20,
-                    )
-                    return get!(
-                        solutions_for_rooting_depth,
-                        rooting_depth,
+                #=======================
+                Here we solve for the steady state of the hydraulics
+                system. Then, the solution is used to check that evaluating the
+                tendecy of the model also results in a steady state. This check is repeated using
+                the plant hydraulics model directly.
+                =======================#
+
+                soln = nlsolve(
+                    initial_compute_exp_tendency!,
+                    Vector{FT}(-0.03:0.01:0.07);
+                    ftol = eps(FT),
+                    method = :newton,
+                    iterations = 20,
+                )
+
+                S_l =
+                    inverse_water_retention_curve.(
+                        retention_model,
                         soln.zero,
-                    )[i]
+                        plant_ν,
+                        plant_S_s,
+                    )
+
+                ϑ_l_0 = augmented_liquid_fraction.(plant_ν, S_l)
+
+                Y, p, coords = initialize(model)
+                if typeof(domain) <: ClimaLand.Domains.Point
+                    @test propertynames(p) == (:canopy, :drivers)
+                elseif typeof(domain) <: ClimaLand.Domains.Plane
+                    @test propertynames(p) ==
+                          (:canopy, :dss_buffer_2d, :drivers)
+                    @test typeof(p.dss_buffer_2d) == typeof(
+                        ClimaCore.Spaces.create_dss_buffer(
+                            ClimaCore.Fields.zeros(domain.space.surface),
+                        ),
+                    )
                 end
-            end
-            # instead of a field of vectors, we have a vector of fields
-            soln_field_vector = map(1:(n_stem + n_leaf)) do i
-                solve_sys_for_i.(plant_hydraulics.parameters.rooting_depth, i)
-            end
 
-            S_l = map(soln_field_vector) do soln_i_field
-                inverse_water_retention_curve.(
-                    retention_model,
-                    soln_i_field,
-                    plant_ν,
-                    plant_S_s,
+                dY = similar(Y)
+                for i in 1:(n_stem + n_leaf)
+                    Y.canopy.hydraulics.ϑ_l.:($i) .= ϑ_l_0[i]
+                    p.canopy.hydraulics.ψ.:($i) .= NaN
+                    p.canopy.hydraulics.fa.:($i) .= NaN
+                    dY.canopy.hydraulics.ϑ_l.:($i) .= NaN
+                end
+                set_initial_cache! = make_set_initial_cache(model)
+                set_initial_cache!(p, Y, 0.0)
+                canopy_exp_tendency! = make_exp_tendency(model)
+                canopy_exp_tendency!(dY, Y, p, 0.0)
+
+                tolerance = sqrt(eps(FT))
+                @test all(parent(dY.canopy.hydraulics.ϑ_l) .< tolerance) # starts in equilibrium
+
+
+                # repeat using the plant hydraulics model directly
+                # make sure it agrees with what we get when use the canopy model ODE
+                Y, p, coords = initialize(model)
+                standalone_dY = similar(Y)
+                for i in 1:(n_stem + n_leaf)
+                    Y.canopy.hydraulics.ϑ_l.:($i) .= ϑ_l_0[i]
+                    p.canopy.hydraulics.ψ.:($i) .= NaN
+                    p.canopy.hydraulics.fa.:($i) .= NaN
+                    standalone_dY.canopy.hydraulics.ϑ_l.:($i) .= NaN
+                end
+                set_initial_cache!(p, Y, 0.0)
+                standalone_exp_tendency! =
+                    make_compute_exp_tendency(model.hydraulics, model)
+                standalone_exp_tendency!(standalone_dY, Y, p, 0.0)
+                @test all(
+                    parent(
+                        standalone_dY.canopy.hydraulics.ϑ_l .-
+                        dY.canopy.hydraulics.ϑ_l,
+                    ) .≈ FT(0),
                 )
             end
-
-            ϑ_l_0 = map(S_l) do S_l_i
-                augmented_liquid_fraction.(plant_ν, S_l_i)
-            end
-
-            Y, p, coords = initialize(model)
-            if typeof(domain) <: ClimaLand.Domains.Point
-                @test propertynames(p) == (:canopy, :drivers)
-            elseif typeof(domain) <: ClimaLand.Domains.Plane
-                @test propertynames(p) == (:canopy, :dss_buffer_2d, :drivers)
-                @test typeof(p.dss_buffer_2d) == typeof(
-                    ClimaCore.Spaces.create_dss_buffer(
-                        ClimaCore.Fields.zeros(domain.space.surface),
-                    ),
-                )
-            end
-
-            dY = similar(Y)
-            for i in 1:(n_stem + n_leaf)
-                Y.canopy.hydraulics.ϑ_l.:($i) .= ϑ_l_0[i]
-                p.canopy.hydraulics.ψ.:($i) .= NaN
-                p.canopy.hydraulics.fa.:($i) .= NaN
-                dY.canopy.hydraulics.ϑ_l.:($i) .= NaN
-            end
-            set_initial_cache! = make_set_initial_cache(model)
-            set_initial_cache!(p, Y, 0.0)
-            canopy_exp_tendency! = make_exp_tendency(model)
-            canopy_exp_tendency!(dY, Y, p, 0.0)
-
-            tolerance = sqrt(eps(FT))
-            @test all(parent(dY.canopy.hydraulics.ϑ_l) .< tolerance) # starts in equilibrium
-
-
-            # repeat using the plant hydraulics model directly
-            # make sure it agrees with what we get when use the canopy model ODE
-            Y, p, coords = initialize(model)
-            standalone_dY = similar(Y)
-            for i in 1:(n_stem + n_leaf)
-                Y.canopy.hydraulics.ϑ_l.:($i) .= ϑ_l_0[i]
-                p.canopy.hydraulics.ψ.:($i) .= NaN
-                p.canopy.hydraulics.fa.:($i) .= NaN
-                standalone_dY.canopy.hydraulics.ϑ_l.:($i) .= NaN
-            end
-            set_initial_cache!(p, Y, 0.0)
-            standalone_exp_tendency! =
-                make_compute_exp_tendency(model.hydraulics, model)
-            standalone_exp_tendency!(standalone_dY, Y, p, 0.0)
-            @test all(
-                parent(
-                    standalone_dY.canopy.hydraulics.ϑ_l .-
-                    dY.canopy.hydraulics.ϑ_l,
-                ) .≈ FT(0),
-            )
         end
     end
 
