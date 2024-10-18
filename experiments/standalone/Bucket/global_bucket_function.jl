7304b5abd2df2500e5afd3fb097d350a6a8a5863
Gabriele Bozzola <sbozzolator@gmail.com>
2024-10-18 10:06:34 -0700

    Use ClimaDiagnostics in bucket experiments

--- experiments/standalone/Bucket/global_bucket_function.jl
+++ experiments/standalone/Bucket/global_bucket_function.jl
@@ -63,11 +63,14 @@ anim_plots = false
 FT = Float64;
 context = ClimaComms.context()
 earth_param_set = LP.LandParameters(FT);
+# Use separate output directory for CPU and GPU runs to avoid race condition
+device_suffix =
+    typeof(ClimaComms.context().device) <: ClimaComms.CPUSingleThreaded ?
+    "cpu" : "gpu"
 outdir = joinpath(
     pkgdir(ClimaLand),
-    "experiments/standalone/Bucket/artifacts_function",
+    "experiments/standalone/Bucket/artifacts_function_$(device_suffix)",
 )
-!ispath(outdir) && mkpath(outdir)
 
 # Construct simulation domain
 soil_depth = FT(3.5);
@@ -230,9 +233,6 @@ T_sfc = Array(Remapping.interpolate(remapper, prob.p.bucket.T_sfc))
 
 # save prognostic state to CSV - for comparison between
 # GPU and CPU output
-device_suffix =
-    typeof(ClimaComms.context().device) <: ClimaComms.CPUSingleThreaded ?
-    "cpu" : "gpu"
-open(joinpath(outdir, "tf_state_$(device_suffix)_function.txt"), "w") do io
+open(joinpath(output_dir, "tf_state_$(device_suffix)_function.txt"), "w") do io
     writedlm(io, hcat(T_sfc[:], W[:], Ws[:], σS[:]), ',')
 end;
