82cb229d8c37a19b707cf4a51c84a522e75280b9
Julia Sloan <jsloan@caltech.edu>
2025-01-16 13:44:37 -0800

    run soilcanopy and snowy land longruns for 3 months

--- experiments/long_runs/snowy_land.jl
+++ experiments/long_runs/snowy_land.jl
@@ -406,7 +406,7 @@ end
 function setup_and_solve_problem(; greet = false)
 
     t0 = 0.0
-    tf = 60 * 60.0 * 24 * 365 * 2
+    tf = 60 * 60.0 * 24 * 93#365 * 2
     Δt = 450.0
     nelements = (101, 15)
     if greet
