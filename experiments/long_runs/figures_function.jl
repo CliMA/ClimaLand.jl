3850aae3c8860fc3444827a191df6d6f7bbe890a
Julia Sloan <jsloan@caltech.edu>
2025-02-07 15:51:13 -0800

    working on seasonal cycle now

--- experiments/long_runs/figures_function.jl
+++ experiments/long_runs/figures_function.jl
@@ -31,113 +31,113 @@ function make_figures(
         # ]
 
         ## SEASONAL CYCLE
-        # kwarg_z = ClimaAnalysis.has_altitude(var) ? Dict(:z => 1) : Dict() # if has altitude, take first layer
-        # var_sliced = ClimaAnalysis.slice(var; kwarg_z...)
+        kwarg_z = ClimaAnalysis.has_altitude(var) ? Dict(:z => 1) : Dict() # if has altitude, take first layer
+        var_sliced = ClimaAnalysis.slice(var; kwarg_z...)
         # var_global_average below is a vector of vector, one for each year of simulation, containing monthly global average of var.
         # i represent a year, from 1 to last year
         # for more details on the ClimaAnalysis functions, see ClimaAnalysis docs.
-        # var_global_average = [
-        #     ClimaAnalysis.average_lon(
-        #         ClimaAnalysis.weighted_average_lat(
-        #             ClimaAnalysis.apply_oceanmask(
-        #                 ClimaAnalysis.window(
-        #                     var_sliced,
-        #                     "time",
-        #                     left = (i - 1) * 366 * 86400 + 30 * 86400, # 1 year left of year i, in seconds.
-        #                     right = i * 366 * 86400, # 1 year right of year i, in seconds
-        #                 ),
-        #             ),
-        #         ),
-        #     ).data for i in range(
-        #         1,
-        #         round(
-        #             last(ClimaAnalysis.times(var_sliced) / (365 * 86400)),
-        #         ), # n years
-        #     )
-        # ]
+        var_global_average = [
+            ClimaAnalysis.average_lon(
+                ClimaAnalysis.weighted_average_lat(
+                    ClimaAnalysis.apply_oceanmask(
+                        ClimaAnalysis.window(
+                            var_sliced,
+                            "time",
+                            left = (i - 1) * 366 * 86400 + 30 * 86400, # 1 year left of year i, in seconds.
+                            right = i * 366 * 86400, # 1 year right of year i, in seconds
+                        ),
+                    ),
+                ),
+            ).data for i in range(
+                1,
+                round(
+                    last(ClimaAnalysis.times(var_sliced) / (365 * 86400)),
+                ), # n years
+            )
+        ]
 
-        # fig_seasonal_cycle = CairoMakie.Figure(size = (600, 400))
-        # ax = Axis(
-        #     fig_seasonal_cycle[1, 1],
-        #     xlabel = "Month of the year",
-        #     ylabel = "$short_name [$(ClimaAnalysis.units(var))]",
-        #     xticks = (
-        #         1:1:12,
-        #         [
-        #             "Jan",
-        #             "Feb",
-        #             "Mar",
-        #             "Apr",
-        #             "May",
-        #             "Jun",
-        #             "Jul",
-        #             "Aug",
-        #             "Sep",
-        #             "Oct",
-        #             "Nov",
-        #             "Dev",
-        #         ],
-        #     ),
-        # )
-        # [
-        #     CairoMakie.lines!(
-        #         ax,
-        #         var_global_average[i],
-        #         color = RGBf(0.5, 0.5, 0.5),
-        #         linewidth = 1,
-        #         linestyle = (i == 1 ? :dash : :solid), # dashed line for the 1st year
-        #     ) for i in 1:length(var_global_average)
-        # ]
-        # # The three next lines here are computing the average for each month of var, January to December. It accounts for cases if the last simulated year is incomplete. In that case, the last vector of var_global_average would be shorter than 12, so in order to compute the average, it needs to be padded with NaNs until it reaches the length of 12.
-        # # We also want the average for each month to be computed even if there is NaNs in the last vector, for example January may have 3 data points, but December only two, but we still want the average of all months.
-        # max_len = maximum(length.(var_global_average)) # 12 months
-        # padded_var = [
-        #     vcat(v, fill(NaN, max_len - length(v))) for
-        #     v in var_global_average
-        # ] # fill with NaN up to 12, if a vector is shorter
-        # monthly_averages_var = [
-        #     mean(filter(!isnan, collect([v[i] for v in padded_var])))
-        #     for i in 1:max_len
-        # ] # compute average of each month, ignoring potential NaNs
-        # CairoMakie.lines!(
-        #     ax,
-        #     monthly_averages_var,
-        #     color = :black,
-        #     linewidth = 3,
-        # )
-        # CairoMakie.xlims!(ax, 1, 12)
-        # CairoMakie.save(
-        #     joinpath(root_path, "$(short_name)_global_monthly.pdf"),
-        #     fig_seasonal_cycle,
-        # )
+        fig_seasonal_cycle = CairoMakie.Figure(size = (600, 400))
+        ax = Axis(
+            fig_seasonal_cycle[1, 1],
+            xlabel = "Month of the year",
+            ylabel = "$short_name [$(ClimaAnalysis.units(var))]",
+            xticks = (
+                1:1:12,
+                [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dev",
+                ],
+            ),
+        )
+        [
+            CairoMakie.lines!(
+                ax,
+                var_global_average[i],
+                color = RGBf(0.5, 0.5, 0.5),
+                linewidth = 1,
+                linestyle = (i == 1 ? :dash : :solid), # dashed line for the 1st year
+            ) for i in 1:length(var_global_average)
+        ]
+        # The three next lines here are computing the average for each month of var, January to December. It accounts for cases if the last simulated year is incomplete. In that case, the last vector of var_global_average would be shorter than 12, so in order to compute the average, it needs to be padded with NaNs until it reaches the length of 12.
+        # We also want the average for each month to be computed even if there is NaNs in the last vector, for example January may have 3 data points, but December only two, but we still want the average of all months.
+        max_len = maximum(length.(var_global_average)) # 12 months
+        padded_var = [
+            vcat(v, fill(NaN, max_len - length(v))) for
+            v in var_global_average
+        ] # fill with NaN up to 12, if a vector is shorter
+        monthly_averages_var = [
+            mean(filter(!isnan, collect([v[i] for v in padded_var])))
+            for i in 1:max_len
+        ] # compute average of each month, ignoring potential NaNs
+        CairoMakie.lines!(
+            ax,
+            monthly_averages_var,
+            color = :black,
+            linewidth = 3,
+        )
+        CairoMakie.xlims!(ax, 1, 12)
+        CairoMakie.save(
+            joinpath(root_path, "$(short_name)_global_monthly.pdf"),
+            fig_seasonal_cycle,
+        )
 
         ## GLOBAL HEATMAP
-        for t in var_times
-            title = CairoMakie.rich(title_stub, fontsize = 18) # title of the figure
-            fig = CairoMakie.Figure(size = (600, 400))
-            kwargs = ClimaAnalysis.has_altitude(var) ? Dict(:z => 1) : Dict()
-            viz.heatmap2D_on_globe!(
-                fig,
-                ClimaAnalysis.slice(var, time = t; kwargs...),
-                colorbar_label = colorbar_labels[short_name],
-                mask = viz.oceanmask(),
-                more_kwargs = Dict(
-                    :mask => ClimaAnalysis.Utils.kwargs(color = :white),
-                    :plot => ClimaAnalysis.Utils.kwargs(rasterize = true),
-                    :axis => ClimaAnalysis.Utils.kwargs(
-                        title = title,
-                        xticklabelsvisible = false, # don't show lat labels
-                        yticklabelsvisible = false, # don't show lon labels
-                        xgridvisible = false, # don't show lat grid
-                        ygridvisible = false, # don't show lon grid
-                    ),
-                    # :cb => ClimaAnalysis.Utils.kwargs(
-                    #     rightspinevisible = true,
-                    # ),
-                ),
-            )
-            CairoMakie.save(joinpath(root_path, "$(short_name)_$t.pdf"), fig)
-        end
+    #     for t in var_times
+    #         title = CairoMakie.rich(title_stub, fontsize = 18) # title of the figure
+    #         fig = CairoMakie.Figure(size = (600, 400))
+    #         kwargs = ClimaAnalysis.has_altitude(var) ? Dict(:z => 1) : Dict()
+    #         viz.heatmap2D_on_globe!(
+    #             fig,
+    #             ClimaAnalysis.slice(var, time = t; kwargs...),
+    #             colorbar_label = colorbar_labels[short_name],
+    #             mask = viz.oceanmask(),
+    #             more_kwargs = Dict(
+    #                 :mask => ClimaAnalysis.Utils.kwargs(color = :white),
+    #                 :plot => ClimaAnalysis.Utils.kwargs(rasterize = true),
+    #                 :axis => ClimaAnalysis.Utils.kwargs(
+    #                     title = title,
+    #                     xticklabelsvisible = false, # don't show lat labels
+    #                     yticklabelsvisible = false, # don't show lon labels
+    #                     xgridvisible = false, # don't show lat grid
+    #                     ygridvisible = false, # don't show lon grid
+    #                 ),
+    #                 # :cb => ClimaAnalysis.Utils.kwargs(
+    #                 #     rightspinevisible = true,
+    #                 # ),
+    #             ),
+    #         )
+    #         CairoMakie.save(joinpath(root_path, "$(short_name)_$t.pdf"), fig)
+    #     end
     end
     # figures = readdir(root_path, join = true)
     # pdfunite() do unite
