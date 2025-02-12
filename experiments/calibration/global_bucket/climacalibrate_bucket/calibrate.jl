b0f4c6696ab4a6979ba8c3590cb31c291a4420c9
kmdeck <65979205+kmdeck@users.noreply.github.com>
2025-02-12 11:34:20 -0800

    Update calibrate.jl [skip ci]

--- experiments/calibration/global_bucket/climacalibrate_bucket/calibrate.jl
+++ experiments/calibration/global_bucket/climacalibrate_bucket/calibrate.jl
@@ -21,10 +21,10 @@ include(joinpath(dir,"experiments/calibration/global_bucket/climacalibrate_bucke
 
 prior_κ_soil = EKP.constrained_gaussian("κ_soil", 2, 1, 0, Inf);
 prior_ρc_soil = EKP.constrained_gaussian("ρc_soil", 4e6, 2e6, 0, Inf);
-prior_f_bucket = EKP.constrained_gaussian("f_bucket", 0.5, 0.3, 0, 1);
-prior_W_f = EKP.constrained_gaussian("W_f", 0.4, 0.4, 0, Inf);
+prior_f_bucket = EKP.constrained_gaussian("f_bucket", 0.6, 0.2, 0, 1);
+prior_W_f = EKP.constrained_gaussian("W_f", 0.25, 0.2, 0, Inf);
 prior_p = EKP.constrained_gaussian("p", 2, 1, 1, Inf);
-prior_z_0m = EKP.constrained_gaussian("z_0m", 0.01, 0.1, 0, Inf);
+prior_z_0m = EKP.constrained_gaussian("z_0m", 0.01, 0.04, 0, Inf);
 prior = EKP.combine_distributions([
     prior_κ_soil,
     prior_ρc_soil,
