17abc4f846a52c644b6b7bc11ff2762495c47095
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-02-14 13:05:47 -0800

    Working snowy land long run with ITime

--- experiments/long_runs/snowy_land.jl
+++ experiments/long_runs/snowy_land.jl
@@ -22,6 +22,7 @@ import ClimaTimeSteppers as CTS
 using ClimaCore
 using ClimaUtilities.ClimaArtifacts
 import ClimaUtilities.OnlineLogging: WallTimeInfo, report_walltime
+import ClimaUtilities.TimeManager: ITime
 
 import ClimaDiagnostics
 import ClimaAnalysis
@@ -60,7 +61,14 @@ diagnostics_outdir = joinpath(root_path, "global_diagnostics")
 outdir =
     ClimaUtilities.OutputPathGenerator.generate_output_path(diagnostics_outdir)
 
-function setup_prob(t0, tf, Δt; outdir = outdir, nelements = (101, 15))
+function setup_prob(
+    t0,
+    tf,
+    Δt,
+    start_date;
+    outdir = outdir,
+    nelements = (101, 15),
+)
     earth_param_set = LP.LandParameters(FT)
     radius = FT(6378.1e3)
     depth = FT(50)
@@ -74,7 +82,6 @@ function setup_prob(t0, tf, Δt; outdir = outdir, nelements = (101, 15))
     surface_space = domain.space.surface
     subsurface_space = domain.space.subsurface
 
-    start_date = DateTime(2008)
     # Forcing data
     era5_artifact_path =
         ClimaLand.Artifacts.era5_land_forcing_data2008_folder_path(; context)
@@ -365,7 +372,7 @@ function setup_prob(t0, tf, Δt; outdir = outdir, nelements = (101, 15))
         p,
     )
 
-    updateat = Array(t0:(3600 * 3):tf)
+    updateat = [promote(t0:(ITime(3600 * 3)):tf...)...]
     drivers = ClimaLand.get_drivers(land)
     updatefunc = ClimaLand.make_update_drivers(drivers)
 
@@ -419,6 +426,7 @@ function setup_and_solve_problem(; greet = false)
     years = 366days # years in seconds - 366 to make sure we capture at least full years
     tf = 2years # 2 years in seconds
     Δt = 450.0
+    start_date = DateTime(2008)
     nelements = (101, 15)
     if greet
         @info "Run: Global Soil-Canopy-Snow Model"
@@ -427,7 +435,11 @@ function setup_and_solve_problem(; greet = false)
         @info "Duration: $(tf - t0) s"
     end
 
-    prob, cb = setup_prob(t0, tf, Δt; nelements)
+    t0 = ITime(t0, epoch = start_date)
+    tf = ITime(tf, epoch = start_date)
+    Δt = ITime(Δt, epoch = start_date)
+    t0, tf, Δt = promote(t0, tf, Δt)
+    prob, cb = setup_prob(t0, tf, Δt, start_date; nelements)
 
     # Define timestepper and ODE algorithm
     stepper = CTS.ARS111()
