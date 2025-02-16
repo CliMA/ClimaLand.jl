5d41ec4ce521a11fd1258378a5c15774c2caed7d
Edward Speer <espeer@caltech.edu>
2025-02-16 07:31:25 -0800

    Repair broadcast inference

--- test/standalone/Vegetation/test_two_stream.jl
+++ test/standalone/Vegetation/test_two_stream.jl
@@ -26,12 +26,13 @@ for FT in (Float32, Float64)
     @testset "Two-Stream Model Correctness, FT = $FT" begin
         # Use a single band for the test
         spectral_discretization = TwoBandSpectralDiscretization{FT}()
+        ε_canopy = FT(0.99)
 
         # Read the conditions for each setup parameter from the test file
         column_names = test_set[1, :]
         θs = acos.(FT.(test_set[2:end, column_names .== "mu"]))
         LAI = FT.(test_set[2:end, column_names .== "LAI"])
-        a_soil = FT.(test_set[2:end, column_names .== "a_soil"])
+        a_soil_vals = FT.(test_set[2:end, column_names .== "a_soil"])
         n_layers = UInt64.(test_set[2:end, column_names .== "n_layers"])
         PropDif = FT.(test_set[2:end, column_names .== "prop_diffuse"])
 
@@ -39,28 +40,30 @@ for FT in (Float32, Float64)
         domain = Point(; z_sfc = FT(0.0))
         lds = FT.(test_set[2:end, column_names .== "ld"])
         lds_field = map(x -> fill(x, domain.space.surface), lds)
-        ρ_PAR_leaf_scalars = FT.(test_set[2:end, column_names .== "rho"])
+        a_soil_scalars = map(x -> FT.((x, x)), a_soil_vals)
+        a_soil_fields =
+            map(x -> fill(FT.((x, x)), domain.space.surface), a_soil_vals)
+        a_soil_cases = (a_soil_scalars, a_soil_fields)
+        ρ_PAR_leaf_scalar_vals = FT.(test_set[2:end, column_names .== "rho"])
+        ρ_PAR_leaf_scalars = map(x -> (x, x), ρ_PAR_leaf_scalar_vals)
         ρ_PAR_leaf_fields =
-            map(x -> fill(x, domain.space.surface), ρ_PAR_leaf_scalars)
-        τ_scalars = FT.(test_set[2:end, column_names .== "tau"])
-        τ_fields = map(x -> fill(x, domain.space.surface), τ_scalars)
+            map(x -> fill((x, x), domain.space.surface), ρ_PAR_leaf_scalar_vals)
+        τ_scalar_vals = FT.(test_set[2:end, column_names .== "tau"])
+        τ_scalars = map(x -> (x, x), τ_scalar_vals)
+        τ_fields = map(x -> fill((x, x), domain.space.surface), τ_scalar_vals)
         # loop through once with params as floats, then with params as fields
         Ω_cases = (FT(1), fill(FT(1), domain.space.surface))
         ρ_PAR_leaf_cases = (ρ_PAR_leaf_scalars, ρ_PAR_leaf_fields)
         τ_PAR_leaf_cases = (τ_scalars, τ_fields)
-        ρ_NIR_leaf_cases = (FT(0.4), fill(FT(0.4), domain.space.surface))
-        τ_NIR_leaf_cases = (FT(0.25), fill(FT(0.24), domain.space.surface))
         lds_cases = (lds, lds_field)
         zipped_params = zip(
             Ω_cases,
             ρ_PAR_leaf_cases,
             τ_PAR_leaf_cases,
-            ρ_NIR_leaf_cases,
-            τ_NIR_leaf_cases,
             lds_cases,
+            a_soil_cases,
         )
-        for (Ω, ρ_PAR_leaf, τ_PAR_leaf, ρ_NIR_leaf, τ_NIR_leaf, lds) in
-            zipped_params
+        for (Ω, ρ_PAR_leaf, τ_PAR_leaf, lds, a_soil) in zipped_params
             # Read the result for each setup from the Python output
             py_FAPAR = FT.(test_set[2:end, column_names .== "FAPAR"])
 
@@ -69,13 +72,14 @@ for FT in (Float32, Float64)
             for i in 2:(size(test_set, 1) - 1)
 
                 # Set the parameters based on the setup read from the file
-                RT_params = TwoStreamParameters(
-                    FT;
+                RT_params = TwoStreamParameters(;
                     spectral_discretization = spectral_discretization,
                     Ω = Ω,
                     G_Function = ConstantGFunction(FT.(lds[i])),
-                    ρ_leaf = (ρ_PAR_leaf[i], ρ_NIR_leaf[i]),
-                    τ_leaf = (τ_PAR_leaf[i], τ_NIR_leaf[i]),
+                    ρ_leaf = ρ_PAR_leaf[i],
+                    τ_leaf = τ_PAR_leaf[i],
+                    ϵ_canopy = FT(0.0), # Unused
+                    λ_γ_PAR = FT(0.0),  # Unusedsp
                     n_layers = n_layers[i],
                 )
 
@@ -85,20 +89,47 @@ for FT in (Float32, Float64)
                 # Compute the predicted FAPAR using the ClimaLand TwoStream implementation
                 G = compute_G(RT_params.G_Function, θs)
                 K = extinction_coeff.(G, θs[i])
-                output =
-                    canopy_sw_rt_two_stream.(
+                if typeof(RT_params.ρ_leaf) <: Tuple
+                    output = canopy_sw_rt_two_stream(
                         G,
                         RT_params.Ω,
                         RT_params.n_layers,
-                        RT_params.ρ_leaf[1],
-                        RT_params.τ_leaf[1],
+                        RT_params.ρ_leaf,
+                        RT_params.τ_leaf,
                         LAI[i],
                         K,
                         θs[i],
                         a_soil[i],
                         PropDif[i],
                     )
-                FAPAR = output.abs
+                else
+                    output = fill(
+                        (
+                            (abs = FT(0.0), refl = FT(0.0), trans = FT(0.0)),
+                            (abs = FT(0.0), refl = FT(0.0), trans = FT(0.0)),
+                        ),
+                        domain.space.surface,
+                    )
+                    output .=
+                        canopy_sw_rt_two_stream.(
+                            G,
+                            RT_params.Ω,
+                            RT_params.n_layers,
+                            RT_params.ρ_leaf,
+                            RT_params.τ_leaf,
+                            LAI[i],
+                            K,
+                            θs[i],
+                            a_soil[i],
+                            PropDif[i],
+                        )
+                end
+                if typeof(output) <: Tuple
+                    FAPAR = output[1].abs
+                else
+                    get_abs = (x) -> x[1].abs
+                    FAPAR = get_abs.(output)
+                end
                 # Check that the predictions are app. equivalent to the Python model
                 # Create a field of the expect value because isapprox cannot be broadcast
                 # over a field of floats. The domain is a point, so it makes no difference
