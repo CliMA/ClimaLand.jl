b25daf0cdc4d983d7f735585d3d17ab61f0deabc
ph-kev <98072684+ph-kev@users.noreply.github.com>
2025-03-17 16:58:09 -0700

    Update leaderboard for longer run of snowy land

--- experiments/long_runs/leaderboard/leaderboard.jl
+++ experiments/long_runs/leaderboard/leaderboard.jl
@@ -29,7 +29,9 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
     @info "Error against observations"
 
     # Set up dict for storing simulation and observational data after processing
+    # and for storing the month we are interested in
     sim_obs_comparsion_dict = Dict()
+    times_dict = Dict() # map short name to (times, dates)
 
     # Print dates for debugging
     _, var_func = first(sim_var_dict)
@@ -57,38 +59,67 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
                 ClimaAnalysis.window(sim_var, "time", left = spinup_cutoff)
         )
 
+        # Determine which 12 months to compute RMSE on
+        # For a very long run, there is a possibility of the final year of the observational
+        # data to be earlier than the final year of the simulation data
+        sim_times = ClimaAnalysis.times(sim_var)
+        obs_times = ClimaAnalysis.times(obs_var)
+        sim_time_idx = max(length(sim_times) - 11, 1)
+        obs_time_idx = max(length(obs_times) - 11, 1)
+        if obs_times[obs_time_idx] < sim_times[sim_time_idx]
+            left_time = obs_times[obs_time_idx]
+            right_time = obs_times[end]
+            times = obs_times[obs_time_idx:end]
+            dates =
+                Dates.DateTime(obs_var.attributes["start_date"]) .+
+                Dates.Second.(times)
+        else
+            left_time = sim_times[sim_time_idx]
+            right_time = sim_times[end]
+            times = sim_times[sim_time_idx:end]
+            dates =
+                Dates.DateTime(sim_var.attributes["start_date"]) .+
+                Dates.Second.(times)
+        end
+
         # Get 12 or less months of data
-        num_times = length(ClimaAnalysis.times(sim_var))
-        num_times > 12 && (
-            sim_var = ClimaAnalysis.window(
-                sim_var,
-                "time",
-                right = ClimaAnalysis.times(sim_var)[12],
-            )
+        sim_var = ClimaAnalysis.window(
+            sim_var,
+            "time",
+            left = times[begin],
+            right = times[end],
+        )
+        obs_var = ClimaAnalysis.window(
+            obs_var,
+            "time",
+            left = times[begin],
+            right = times[end],
         )
 
         obs_var = ClimaAnalysis.resampled_as(obs_var, sim_var)
 
         sim_obs_comparsion_dict[short_name] = (sim_var, obs_var)
+        times_dict[short_name] = (times, dates)
     end
-
-    # Get the right number of months for plotting
-    _, sim_obs_tuple = first(sim_obs_comparsion_dict)
-    num_times = length(ClimaAnalysis.times(sim_obs_tuple[1]))
-    months = Dates.monthname.(1:num_times)
 
     # Plot monthly comparsions
     for short_name in short_names
-        fig = CairoMakie.Figure(size = (650 * ceil(num_times / 2), 450 * 2))
+        # Get the right number of months for plotting
         sim_var, obs_var = sim_obs_comparsion_dict[short_name]
+        times, monthly_dates = times_dict[short_name]
+        num_times = length(monthly_dates)
+        months_and_years = (
+            (Dates.monthname(date), Dates.year(date)) for date in monthly_dates
+        )
+
+        fig = CairoMakie.Figure(size = (650 * ceil(num_times / 2), 450 * 2))
         mask = mask_dict[short_name](sim_var, obs_var)
-        times = ClimaAnalysis.times(sim_var) |> copy
         times = vcat(
             times,
             Array{Union{Missing, eltype(times)}}(missing, 12 - num_times),
         )
         times = reshape(times, (2, 6))
-        for ((indices, t), month) in zip(pairs(times), months)
+        for ((indices, t), (month, year)) in zip(pairs(times), months_and_years)
             layout = fig[Tuple(indices)...] = CairoMakie.GridLayout()
             ClimaAnalysis.Visualize.plot_bias_on_globe!(
                 layout,
@@ -99,7 +130,7 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
             )
             CairoMakie.Label(
                 layout[0, 1],
-                month,
+                month * " $year",
                 tellwidth = false,
                 fontsize = 30,
             )
@@ -114,11 +145,13 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
     fig = CairoMakie.Figure(size = (450 * length(keys(sim_var_dict)), 600))
     for (col, short_name) in enumerate(keys(sim_var_dict))
         sim_var, obs_var = sim_obs_comparsion_dict[short_name]
+        times, monthly_dates = times_dict[short_name]
+        years = unique(Dates.year.(monthly_dates))
+        num_times = length(times)
         mask = mask_dict[short_name](sim_var, obs_var)
         # Compute globas bias and global rmse
         rmse_vec = []
         bias_vec = []
-        times = ClimaAnalysis.times(sim_var) |> copy
         for t in times
             g_rmse = ClimaAnalysis.global_rmse(
                 ClimaAnalysis.slice(sim_var, time = t),
@@ -136,11 +169,11 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
         ax_rmse = CairoMakie.Axis(
             fig[1, col],
             title = "Global RMSE for $short_name",
-            xlabel = "Month",
+            xlabel = "Month and Year: $years",
             ylabel = "Global RMSE ($(ClimaAnalysis.units(sim_var)))",
             xticks = (
                 1:num_times,
-                Dates.monthname.(1:num_times) .|> x -> x[1:3],
+                Dates.monthname.(monthly_dates) .|> x -> x[1:3],
             ),
         )
         CairoMakie.lines!(ax_rmse, 1:num_times |> collect, rmse_vec)
@@ -149,11 +182,11 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
         ax_bias = CairoMakie.Axis(
             fig[2, col],
             title = "Global Bias for $short_name",
-            xlabel = "Month",
+            xlabel = "Month and Year: $years",
             ylabel = "Global Bias ($(ClimaAnalysis.units(sim_var)))",
             xticks = (
                 1:num_times,
-                Dates.monthname.(1:num_times) .|> x -> x[1:3],
+                Dates.monthname.(monthly_dates) .|> x -> x[1:3],
             ),
         )
         CairoMakie.lines!(ax_bias, 1:num_times |> collect, bias_vec)
@@ -166,7 +199,7 @@ function compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    if length(ARGS) < 2
+    if length(ARGS) != 2
         error(
             "Usage: julia leaderboard.jl <Filepath to save leaderboard and bias plots> <Filepath to simulation data>",
         )
