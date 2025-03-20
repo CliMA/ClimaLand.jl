169af55d92c152321cee5676e49f4e9279493f39
imreddyTeja <treddy0205@gmail.com>
2025-03-20 11:14:57 -0700

    try again...

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,74 +50,74 @@ end
     include("standalone/Snow/tool_tests.jl")
 end
 
-# Standalone Soil model tests
-@safetestset "Soil Biogeochemistry module tests" begin
-    include("standalone/Soil/Biogeochemistry/biogeochemistry_module.jl")
-end
-@safetestset "Soil CO2 parameterization tests" begin
-    include("standalone/Soil/Biogeochemistry/co2_parameterizations.jl")
-end
-@safetestset "Soil climate drivers tests" begin
-    include("standalone/Soil/climate_drivers.jl")
-end
-@safetestset "Soil runoff tests" begin
-    include("standalone/Soil/runoff.jl")
-end
-@safetestset "Soil boundary condition tests" begin
-    include("standalone/Soil/soil_bc.jl")
-end
-@safetestset "Soil parameterization tests" begin
-    include("standalone/Soil/soil_parameterizations.jl")
-end
-@safetestset "Soil 3D domain tests" begin
-    include("standalone/Soil/soil_test_3d.jl")
-end
-@safetestset "Soil integration tests" begin
-    include("standalone/Soil/soiltest.jl")
-end
+# # Standalone Soil model tests
+# @safetestset "Soil Biogeochemistry module tests" begin
+#     include("standalone/Soil/Biogeochemistry/biogeochemistry_module.jl")
+# end
+# @safetestset "Soil CO2 parameterization tests" begin
+#     include("standalone/Soil/Biogeochemistry/co2_parameterizations.jl")
+# end
+# @safetestset "Soil climate drivers tests" begin
+#     include("standalone/Soil/climate_drivers.jl")
+# end
+# @safetestset "Soil runoff tests" begin
+#     include("standalone/Soil/runoff.jl")
+# end
+# @safetestset "Soil boundary condition tests" begin
+#     include("standalone/Soil/soil_bc.jl")
+# end
+# @safetestset "Soil parameterization tests" begin
+#     include("standalone/Soil/soil_parameterizations.jl")
+# end
+# @safetestset "Soil 3D domain tests" begin
+#     include("standalone/Soil/soil_test_3d.jl")
+# end
+# @safetestset "Soil integration tests" begin
+#     include("standalone/Soil/soiltest.jl")
+# end
 
-# Standalone Surface Water model tests
-@safetestset "Pond module tests" begin
-    include("standalone/SurfaceWater/pond_test.jl")
-end
+# # Standalone Surface Water model tests
+# @safetestset "Pond module tests" begin
+#     include("standalone/SurfaceWater/pond_test.jl")
+# end
 
-# Standalone Vegetation model tests
-@safetestset "Canopy module tests" begin
-    include("standalone/Vegetation/canopy_model.jl")
-end
-@safetestset "Canopy PlantHydraulics tests" begin
-    include("standalone/Vegetation/plant_hydraulics_test.jl")
-end
-@safetestset "Big Leaf model tests" begin
-    include("standalone/Vegetation/test_bigleaf_parameterizations.jl")
-end
-@safetestset "Two Stream model tests" begin
-    include("standalone/Vegetation/test_two_stream.jl")
-end
+# # Standalone Vegetation model tests
+# @safetestset "Canopy module tests" begin
+#     include("standalone/Vegetation/canopy_model.jl")
+# end
+# @safetestset "Canopy PlantHydraulics tests" begin
+#     include("standalone/Vegetation/plant_hydraulics_test.jl")
+# end
+# @safetestset "Big Leaf model tests" begin
+#     include("standalone/Vegetation/test_bigleaf_parameterizations.jl")
+# end
+# @safetestset "Two Stream model tests" begin
+#     include("standalone/Vegetation/test_two_stream.jl")
+# end
 
-# Integrated LSM tests
-@safetestset "Integrated LSM unit tests" begin
-    include("integrated/lsms.jl")
-end
-@safetestset "Integrated LSM unit tests" begin
-    include("integrated/soil_canopy_lsm.jl")
-end
-@safetestset "Integrated pond/soil LSM tests" begin
-    include("integrated/pond_soil_lsm.jl")
-end
-@safetestset "Integrated soil energy/hydrology/biogeochem LSM tests" begin
-    include("integrated/soil_energy_hydrology_biogeochemistry.jl")
-end
-@safetestset "Integrated soil and snow" begin
-    include("integrated/soil_snow.jl")
-end
+# # Integrated LSM tests
+# @safetestset "Integrated LSM unit tests" begin
+#     include("integrated/lsms.jl")
+# end
+# @safetestset "Integrated LSM unit tests" begin
+#     include("integrated/soil_canopy_lsm.jl")
+# end
+# @safetestset "Integrated pond/soil LSM tests" begin
+#     include("integrated/pond_soil_lsm.jl")
+# end
+# @safetestset "Integrated soil energy/hydrology/biogeochem LSM tests" begin
+#     include("integrated/soil_energy_hydrology_biogeochemistry.jl")
+# end
+# @safetestset "Integrated soil and snow" begin
+#     include("integrated/soil_snow.jl")
+# end
 
-# Simulations
-@safetestset "Default parameters" begin
-    include("simulations/spatial_parameters.jl")
-end
+# # Simulations
+# @safetestset "Default parameters" begin
+#     include("simulations/spatial_parameters.jl")
+# end
 
-# Diagnostics
-@safetestset "Diagnostics" begin
-    include("diagnostics/diagnostics_tests.jl")
-end
+# # Diagnostics
+# @safetestset "Diagnostics" begin
+#     include("diagnostics/diagnostics_tests.jl")
+# end
