8f6b7e659c65714a40d1f71a52fddb084a6064db
kmdeck <kdeck@caltech.edu>
2025-03-27 08:29:34 -0700

    fix bug

--- src/diagnostics/land_compute_methods.jl
+++ src/diagnostics/land_compute_methods.jl
@@ -213,7 +213,7 @@ function compute_10cm_water_mass!(
     Hθ = p.soil.sub_sfc_scratch
     z = land_model.soil.domain.fields.z
     depth = FT(-0.1)
-    earth_param_set = land.soil.parameters.earth_param_set
+    earth_param_set = land_model.soil.parameters.earth_param_set
     _ρ_liq = LP.ρ_cloud_liq(earth_param_set)
     _ρ_ice = LP.ρ_cloud_ice(earth_param_set)
     @. Hθ = (Y.soil.ϑ_l * _ρ_liq + Y.soil.θ_i * _ρ_ice) * heaviside(z, depth)
