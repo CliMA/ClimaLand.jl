1c5d5d2fdf20aa756fc6c9b87eb7dbe51a1431c3
Alexis Renchon <a.renchon@gmail.com>
2025-04-02 09:31:00 -0700

    make get_path a function

--- experiments/integrated/fluxnet/path_to_data_file.jl
+++ experiments/integrated/fluxnet/path_to_data_file.jl
@@ -1,14 +1,15 @@
-filename = ARGS[1]
+function get_path(site_ID)
+    directories = readdir("/groups/esm/ClimaArtifacts/artifacts/fluxnet2015/")
 
-directories = readdir("/../../groups/esm/ClimaArtifacts/artifacts/fluxnet2015/")
+    for directory in directories
+        if occursin(site_ID, directory)
+            parts = split(directory, r"FULLSET_", limit=2)
 
-for directory in directories
-  if occursin(filename, directory)
-    parts = split(directory, r"FULLSET_", limit=2)
+            parts[1] *= "FULLSET"
+            ret_str = "/groups/esm/ClimaArtifacts/artifacts/fluxnet2015/" * directory * "/" * parts[1] * "_MM_" * parts[2] * ".csv"
+            println(ret_str)
+            break
+        end
+    end
 
-    parts[1] *= "FULLSET"
-    ret_str = "/groups/esm/ClimaArtifacts/artifacts/fluxnet2015/" * directory * "/" * parts[1] * "_MM_" * parts[2] * ".csv"
-    println(ret_str)
-    break
-  end
 end
