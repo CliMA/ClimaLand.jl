a4b9413cf2d514381f237d8a9b2ee1a5d06f4e9e
kmdeck <kdeck@caltech.edu>
2025-05-07 10:12:40 -0700

    run with smaller dt

--- experiments/long_runs/snowy_land.jl
+++ experiments/long_runs/snowy_land.jl
@@ -467,7 +467,7 @@ function setup_and_solve_problem(; greet = false)
     years = 366days # years in seconds - 366 to make sure we capture at least full years
     # 10 years in seconds for very long run and 2 years in seconds otherwise
     tf = LONGER_RUN ? 10years : 1years
-    Δt = 900.0
+    Δt = 225.0
     start_date = LONGER_RUN ? DateTime(2004) : DateTime(2004)
     nelements = (101, 15)
     if greet
