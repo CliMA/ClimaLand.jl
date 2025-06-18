59e937eb4e1f7eac224ff7be4314d6ce7bf79c2d
Katherine Deck <kdeck@derecho6.hsn.de.hpc.ucar.edu>
2025-06-18 17:28:47 -0600

    fix plotting

--- experiments/calibration/calibrate_land.jl
+++ experiments/calibration/calibrate_land.jl
@@ -7,7 +7,7 @@ const spinup_period = Year(1)
 const start_date = DateTime(2008, 12, 01) # this is the start of the forward model spinup
 @assert month(start_date + spinup_period) == 12 "The start of your calibration period should be December."
 const nelements = (50, 15) # resolution - (horizontal elements (lon, lat), vertical elements (soil discretization))
-const dirname = "land_snow_zenith_angle_params_swu" # ideally, describe your calibration in a few words
+const dirname = "land_snow_zenith_angle_params_swu_try2" # ideally, describe your calibration in a few words
 const caldir = joinpath("output", dirname) # you might want to save somewhere else than login
 import ClimaLand
 model_dir = joinpath(pkgdir(ClimaLand), "experiments", "calibration")
@@ -72,24 +72,13 @@ end
 # Locations used for calibration (currently all coordinates on land):
 include(joinpath(model_dir, "make_training_locations.jl"))
 training_locations = make_training_locations(nelements)
-# potentially we can add regional runs or specific lon lat bands or filter (e.g., regions with snow)
-
-# NOTE: The noise is set in observationseries_era5.jl - adjust if needed.
-# ^ current noise options: era5 inter-annual variance, era5 seasonal mean * factor, flat noise, weigh by lats.
-# NOTE: Currently everything is set to use seasonal averages. We could add option to use e.g., monthly or other.
-# NOTE: We could add option to calibrate single sites (change forward model, observationseries, observation_map).
-# ^ maybe Julia and Thanhthanh SURF?
-
-# observationseries - era5 data and noise object to compare to model output in EKP (to minimize the loss)
 include(joinpath(model_dir, "observationseries_era5.jl"))
 
-# l_obs is the length of Observation objects (observationseries for era5, observationmap for ClimaLand)
 n_locations = length(training_locations)
 n_variables = length(variable_list)
 n_time_points = 4 # 4 seasons (and not, for example, 12 months)
 l_obs = n_time_points * n_variables * n_locations
 
-# build observation from ClimaLand outputs - for one member
 include(joinpath(model_dir, "observation_map.jl"))
 
 # build observation from ClimaLand outputs - for all members
@@ -109,17 +98,24 @@ function CAL.observation_map(iteration)
     return G_ensemble
 end
 
-# Plotting helper functions
-include("experiments/long_runs/leaderboard/leaderboard.jl")
+include(
+    joinpath(
+        pkgdir(ClimaLand),
+        "experiments",
+        "long_runs",
+	"leaderboard"
+	"leaderboard.jl"
+    ),
+)
 function CAL.analyze_iteration(ekp, g_ensemble, prior, output_dir, iteration)
     plot_output_path = CAL.path_to_iteration(output_dir, iteration)
 
-    output_path = CAL.path_to_ensemble_member(output_dir, iteration, 1)
+    member1_path = CAL.path_to_ensemble_member(output_dir, iteration, 1)
     diagnostics_folder_path =
-        joinpath(output_path, "global_diagnostics/output_active")
+        joinpath(member1_path, "global_diagnostics/output_0000")
 
     compute_monthly_leaderboard(
-        joinpath(plot_output_path, "leaderboard.jl"),
+        plot_output_path,
         diagnostics_folder_path,
         data_source,
     )
@@ -140,7 +136,6 @@ function plot_constrained_params_and_errors(output_dir, ekp, prior)
     )
     return nothing
 end
-
 # Build the UTKI object - this is where you set EKP configurations
 utki = EKP.EnsembleKalmanProcess(
     observationseries,
