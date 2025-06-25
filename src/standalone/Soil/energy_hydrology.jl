012b1e19de42974196e380b1e11de14d259f6ca8
imreddyTeja <treddy0205@gmail.com>
2025-06-25 15:28:38 -0700

    fixed additive inverse gpu broadcast error

--- src/standalone/Soil/energy_hydrology.jl
+++ src/standalone/Soil/energy_hydrology.jl
@@ -386,7 +386,8 @@ function ClimaLand.make_compute_jacobian(model::EnergyHydrology{FT}) where {FT}
         end
         # dtγ can be an ITime or a float
         @. ∂ϑres∂ϑ =
-            -float(dtγ) *
+            FT(-1) *
+            float(dtγ) *
             (divf2c_matrix() ⋅ p.soil.full_bidiag_matrix_scratch) - (I,)
 
         # Now create the flux term for ∂ρe∂ϑ using bidiag_matrix_scratch
@@ -401,7 +402,8 @@ function ClimaLand.make_compute_jacobian(model::EnergyHydrology{FT}) where {FT}
                 ),
             ) ⋅ p.soil.bidiag_matrix_scratch
         @. ∂ρeres∂ϑ =
-            -float(dtγ) *
+            FT(-1) *
+            float(dtγ) *
             (divf2c_matrix() ⋅ p.soil.full_bidiag_matrix_scratch) - (I,)
 
         # Now overwrite bidiag_matrix_scratch and full_bidiag scratch for the ρe ρe bidiagonal
@@ -418,7 +420,8 @@ function ClimaLand.make_compute_jacobian(model::EnergyHydrology{FT}) where {FT}
             MatrixFields.DiagonalMatrixRow(interpc2f_op(-p.soil.κ)) ⋅
             p.soil.bidiag_matrix_scratch
         @. ∂ρeres∂ρe =
-            -float(dtγ) *
+            FT(-1) *
+            float(dtγ) *
             (divf2c_matrix() ⋅ p.soil.full_bidiag_matrix_scratch) - (I,)
     end
     return compute_jacobian!
