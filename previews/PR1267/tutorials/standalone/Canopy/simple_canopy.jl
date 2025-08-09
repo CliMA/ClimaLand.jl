1504fc16f013d86e0a4109398a4559cfe8d07c6b
Documenter.jl <documenter@juliadocs.github.io>
2025-08-09 03:22:00 +0000

    build based on 9112b46

---
+++ previews/PR1267/tutorials/standalone/Canopy/simple_canopy.jl
@@ -0,0 +1,5 @@
+# # Simple Canopy Model simulation
+# In this tutorial, we'll set up and run a simple canopy model simulation using ClimaLand.jl.
+# We'll use the default parameters and parameterizations, so this is a good starting point for
+# understanding how to set up and run simulations in ClimaLand. This example is analogous
+# to the simple soil example in [Running your first simulation](@ref getting_started.md), but for a canopy model.
