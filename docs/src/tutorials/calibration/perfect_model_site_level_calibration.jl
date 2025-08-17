b193c71e025d95a9f2a4b245107c84accd7ca2b6
nefrathenrici <nat.henrici@gmail.com>
2025-08-17 17:20:47 +0200

    Add observations to final plot

--- docs/src/tutorials/calibration/perfect_model_site_level_calibration.jl
+++ docs/src/tutorials/calibration/perfect_model_site_level_calibration.jl
@@ -346,13 +346,17 @@ for g in eachcol(last_G_ensemble)
     lines!(ax, 1:length(g), g; color = (:blue, 0.6), linewidth = 1.5)
 end
 
+lines!(ax, 1:length(observations), observations; color = (:black, 0.6), linewidth = 3)
+
 axislegend(
     ax,
     [
         LineElement(color = :red, linewidth = 2),
         LineElement(color = :blue, linewidth = 2),
+        LineElement(color = :black, linewidth = 4),
+
     ],
-    ["First iteration", "Last iteration"];
+    ["First ensemble", "Last ensemble", "Observations"];
     position = :rb,
     framevisible = false,
 )
