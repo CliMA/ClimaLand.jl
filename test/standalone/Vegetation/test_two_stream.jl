d285639567d47fd29b62a895603dfe7c3ebd5976
Julia Sloan <jsloan@caltech.edu>
2025-09-15 15:21:09 -0700

    simplify tests to shorten runtime

--- test/standalone/Vegetation/test_two_stream.jl
+++ test/standalone/Vegetation/test_two_stream.jl
@@ -28,93 +28,88 @@ using ClimaCore
     test_set = readdlm(data, ',')
 
     # Floating point precision to use
-    for FT in (Float32, Float64)
-        @testset "Two-Stream Model Correctness, FT = $FT" begin
-            toml_dict = LP.create_toml_dict(FT)
-            # Read the conditions for each setup parameter from the test file
-            column_names = test_set[1, :]
-            cosθs = FT.(test_set[2:end, column_names .== "mu"])
-            LAI = FT.(test_set[2:end, column_names .== "LAI"])
-            a_soil = FT.(test_set[2:end, column_names .== "a_soil"])
-            n_layers = UInt64.(test_set[2:end, column_names .== "n_layers"])
-            PropDif = FT.(test_set[2:end, column_names .== "prop_diffuse"])
+    FT = Float32
+    @testset "Two-Stream Model Correctness, FT = $FT" begin
+        toml_dict = LP.create_toml_dict(FT)
+        # Read the conditions for each setup parameter from the test file
+        column_names = test_set[1, :]
+        cosθs = FT.(test_set[2:end, column_names .== "mu"])
+        LAI = FT.(test_set[2:end, column_names .== "LAI"])
+        a_soil = FT.(test_set[2:end, column_names .== "a_soil"])
+        n_layers = UInt64.(test_set[2:end, column_names .== "n_layers"])
+        PropDif = FT.(test_set[2:end, column_names .== "prop_diffuse"])
 
-            # setup spatially varying params as both float and spatially varying
-            domain = Point(; z_sfc = FT(0.0))
-            lds = FT.(test_set[2:end, column_names .== "ld"])
-            lds_field = map(x -> fill(x, domain.space.surface), lds)
-            α_PAR_leaf_scalars = FT.(test_set[2:end, column_names .== "rho"])
-            α_PAR_leaf_fields =
-                map(x -> fill(x, domain.space.surface), α_PAR_leaf_scalars)
-            τ_scalars = FT.(test_set[2:end, column_names .== "tau"])
-            τ_fields = map(x -> fill(x, domain.space.surface), τ_scalars)
-            # loop through once with params as floats, then with params as fields
-            Ω_cases = (FT(1), fill(FT(1), domain.space.surface))
-            α_PAR_leaf_cases = (α_PAR_leaf_scalars, α_PAR_leaf_fields)
-            τ_PAR_leaf_cases = (τ_scalars, τ_fields)
-            α_NIR_leaf_cases = (FT(0.4), fill(FT(0.4), domain.space.surface))
-            τ_NIR_leaf_cases = (FT(0.25), fill(FT(0.24), domain.space.surface))
-            lds_cases = (lds, lds_field)
-            zipped_params = zip(
-                Ω_cases,
-                α_PAR_leaf_cases,
-                τ_PAR_leaf_cases,
-                α_NIR_leaf_cases,
-                τ_NIR_leaf_cases,
-                lds_cases,
-            )
-            output = ClimaCore.Fields.zeros(
-                @NamedTuple{abs::FT, refl::FT, trans::FT},
-                domain.space.surface,
-            )
-            for (Ω, α_PAR_leaf, τ_PAR_leaf, α_NIR_leaf, τ_NIR_leaf, lds) in
-                zipped_params
-                # Read the result for each setup from the Python output
-                py_FAPAR = FT.(test_set[2:end, column_names .== "FAPAR"])
+        # setup spatially varying params as both float and spatially varying
+        domain = Point(; z_sfc = FT(0.0))
+        lds = FT.(test_set[2:end, column_names .== "ld"])
+        lds_field = map(x -> fill(x, domain.space.surface), lds)
+        α_PAR_leaf_scalars = FT.(test_set[2:end, column_names .== "rho"])
+        α_PAR_leaf_fields =
+            map(x -> fill(x, domain.space.surface), α_PAR_leaf_scalars)
+        τ_scalars = FT.(test_set[2:end, column_names .== "tau"])
+        τ_fields = map(x -> fill(x, domain.space.surface), τ_scalars)
+        # loop through once with params as floats, then with params as fields
+        Ω_cases = (FT(1), fill(FT(1), domain.space.surface))
+        α_PAR_leaf_cases = (α_PAR_leaf_scalars, α_PAR_leaf_fields)
+        τ_PAR_leaf_cases = (τ_scalars, τ_fields)
+        α_NIR_leaf_cases = (FT(0.4), fill(FT(0.4), domain.space.surface))
+        τ_NIR_leaf_cases = (FT(0.25), fill(FT(0.24), domain.space.surface))
+        lds_cases = (lds, lds_field)
+        zipped_params = zip(
+            Ω_cases,
+            α_PAR_leaf_cases,
+            τ_PAR_leaf_cases,
+            α_NIR_leaf_cases,
+            τ_NIR_leaf_cases,
+            lds_cases,
+        )
+        output = ClimaCore.Fields.zeros(
+            @NamedTuple{abs::FT, refl::FT, trans::FT},
+            domain.space.surface,
+        )
+        for (Ω, α_PAR_leaf, τ_PAR_leaf, α_NIR_leaf, τ_NIR_leaf, lds) in
+            zipped_params
+            # Read the result for each setup from the Python output
+            py_FAPAR = FT.(test_set[2:end, column_names .== "FAPAR"])
 
-                # Python code does not use clumping index, and λ_γ does not impact FAPAR
-                # Test over all rows in the stored output from the Python module
-                for i in 2:(size(test_set, 1) - 1)
+            # Python code does not use clumping index, and λ_γ does not impact FAPAR
+            # Test over all rows in the stored output from the Python module
+            for i in 2:(size(test_set, 1) - 1)
 
-                    # Set the parameters based on the setup read from the file
-                    RT_params = TwoStreamParameters(
-                        toml_dict;
-                        Ω = Ω,
-                        G_Function = ConstantGFunction.(FT.(lds[i])),
-                        α_PAR_leaf = α_PAR_leaf[i],
-                        τ_PAR_leaf = τ_PAR_leaf[i],
-                        α_NIR_leaf = α_NIR_leaf,
-                        τ_NIR_leaf = τ_NIR_leaf,
-                        n_layers = n_layers[i],
-                    )
+                # Set the parameters based on the setup read from the file
+                RT_params = TwoStreamParameters(
+                    toml_dict;
+                    Ω = Ω,
+                    G_Function = ConstantGFunction.(FT.(lds[i])),
+                    α_PAR_leaf = α_PAR_leaf[i],
+                    τ_PAR_leaf = τ_PAR_leaf[i],
+                    α_NIR_leaf = α_NIR_leaf,
+                    τ_NIR_leaf = τ_NIR_leaf,
+                    n_layers = n_layers[i],
+                )
 
-                    # Initialize the TwoStream model
-                    RT = TwoStreamModel(RT_params)
+                # Initialize the TwoStream model
+                RT = TwoStreamModel(RT_params)
 
-                    # Compute the predicted FAPAR using the ClimaLand TwoStream implementation
-                    @. output = canopy_sw_rt_two_stream(
-                        RT_params.G_Function,
-                        RT_params.Ω,
-                        RT_params.n_layers,
-                        RT_params.α_PAR_leaf,
-                        RT_params.τ_PAR_leaf,
-                        LAI[i],
-                        cosθs[i],
-                        a_soil[i],
-                        PropDif[i],
-                    )
-                    FAPAR = output.abs
-                    # Check that the predictions are app. equivalent to the Python model
-                    # Create a field of the expect value because isapprox cannot be broadcast
-                    # over a field of floats. The domain is a point, so it makes no difference
-                    # to the error when FAPAR is a float
-                    expected_output = fill(py_FAPAR[i], domain.space.surface)
-                    @test isapprox(
-                        0,
-                        sum(FAPAR .- expected_output),
-                        atol = 0.005,
-                    )
-                end
+                # Compute the predicted FAPAR using the ClimaLand TwoStream implementation
+                @. output = canopy_sw_rt_two_stream(
+                    RT_params.G_Function,
+                    RT_params.Ω,
+                    RT_params.n_layers,
+                    RT_params.α_PAR_leaf,
+                    RT_params.τ_PAR_leaf,
+                    LAI[i],
+                    cosθs[i],
+                    a_soil[i],
+                    PropDif[i],
+                )
+                FAPAR = output.abs
+                # Check that the predictions are app. equivalent to the Python model
+                # Create a field of the expect value because isapprox cannot be broadcast
+                # over a field of floats. The domain is a point, so it makes no difference
+                # to the error when FAPAR is a float
+                expected_output = fill(py_FAPAR[i], domain.space.surface)
+                @test isapprox(0, sum(FAPAR .- expected_output), atol = 0.005)
             end
         end
     end
