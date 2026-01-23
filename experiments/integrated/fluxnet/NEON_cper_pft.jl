d76a1551818a1fa7c21c2a347f41e2b4e92ce99f
Eva-Marie Metz <evametz@caltech.edu>
2026-01-23 11:08:02 -0800

    adapt output path

--- experiments/integrated/fluxnet/NEON_cper_pft.jl
+++ experiments/integrated/fluxnet/NEON_cper_pft.jl
@@ -306,7 +306,7 @@ diags = ClimaLand.default_diagnostics(
 diags = ClimaLand.default_diagnostics(
     land,
     start_date;
-    output_writer = ClimaDiagnostics.Writers.NetCDFWriter(land_domain.space.subsurface, "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Siteruns/NEON-CPER/20260123_6cb1adc3bf4535a0be9244d70cb5026fa677285a/output/"),
+    output_writer = ClimaDiagnostics.Writers.NetCDFWriter(land_domain.space.subsurface, "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Siteruns/NEON-CPER/20260123_5f1577584c0b43c7dcd59a14743ef3ec8c8d5202/output/"),
     output_vars,
     reduction_period = :halfhourly,
 );
