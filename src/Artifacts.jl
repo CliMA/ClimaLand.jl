a4cb4be4898d9ec43a3c2e2a0d6d775f0eb23413
Alexandre A. Renchon <a.renchon@gmail.com>
2026-02-04 15:04:18 -0800

    wip

--- src/Artifacts.jl
+++ src/Artifacts.jl
@@ -312,11 +312,7 @@ AmeriFlux CC-BY-4.0 License
 function experiment_fluxnet_data_path(site_ID; context = nothing)
     @assert site_ID ∈ ("US-MOz", "US-Var", "US-NR1", "US-Ha1","NEON-cper")
     if occursin("NEON", site_ID)
-        #data_path = "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Neon/CliMa_Input/dataframes_Neon/Neon_CliMA_Input_CPER_201701_201712.csv"
-        #data_path = "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Siteruns/NEON-CPER/USVal_param_adaptSlayer_1f8b301b70dc015194659380910d1147f81efba6/input/Neon_CliMA_Input_withERA_wCompData_wSoil_CPER_201701_201712.csv"
-        #data_path = "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Neon/CliMa_Input/dataframes_Neon/Neon_CliMA_Input_withERA_wCompData_wSoil_CPER_201705_201709.csv"
-        data_path = "/home/alexisr/GitHub/ClimaLand.jl/experiments/integrated/fluxnet/Neon_CliMA_Input_withERA_wCompData_wSoil_CPER_201701_201712.csv"
-        #data_path = "/Users/evametz/Documents/PostDoc/Projekte/CliMA/Siteruns/fluxnet_site_data/US-MOz.csv"
+        data_path = "/home/alexis/GitHub/ClimaLand.jl/experiments/integrated/fluxnet/neon/Neon_CliMA_Input_withERA_wCompData_wSoil_CPER_201701_201712.csv"
     else
         folder_path = @clima_artifact("fluxnet_sites", context)
         data_path = joinpath(folder_path, "$(site_ID).csv")
