832b7b1629158f3622a8e94d1bbcde804e3af5f1
Andy Charbonneau <acharbon@caltech.edu>
2026-03-21 00:17:10 -0700

    change bash shell for debug

--- experiments/long_runs/global_thesis_run.sh
+++ experiments/long_runs/global_thesis_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=acharbon_global_20yr_longrun
-#SBATCH --output=output_20yr.txt
-#SBATCH --error=error_20yr.txt
-#SBATCH --time=12:00:00
+#SBATCH --job-name=acharbon_global_debug_1
+#SBATCH --output=output_debug1.txt
+#SBATCH --error=error_debug1.txt
+#SBATCH --time=6:00:00
 #SBATCH --gpus=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mail-user=acharbon@caltech.edu
