b269e713253d97296693deb6dcc4fceeb006e68b
Alexandre A. Renchon <a.renchon@gmail.com>
2026-04-08 10:53:09 -0700

    fix HDF5 file reading issue

--- experiments/calibration/run_calibration.sh
+++ experiments/calibration/run_calibration.sh
@@ -4,6 +4,11 @@
 
 module load climacommon/2025_02_25
 
+# Required on Derecho's Lustre scratch: HDF5 file locking is incompatible with
+# the parallel filesystem and causes NC_EHDFERR (-101) when the orchestrator
+# tries to read NetCDF files written by compute-job ensemble members.
+export HDF5_USE_FILE_LOCKING=FALSE
+
 julia --project=.buildkite -e 'using Pkg; Pkg.instantiate(;verbose=true)'
 julia --project=.buildkite/ experiments/calibration/generate_observations.jl
 julia --project=.buildkite/ experiments/calibration/run_calibration.jl
