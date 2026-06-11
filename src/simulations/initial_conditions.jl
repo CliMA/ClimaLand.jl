acb21e148787623e08f2bc0a04a14134d18ba113
kmdeck <kdeck@caltech.edu>
2026-06-11 14:20:26 -0700

    fixes

--- src/simulations/initial_conditions.jl
+++ src/simulations/initial_conditions.jl
@@ -2,6 +2,8 @@ import ClimaUtilities.SpaceVaryingInputs: SpaceVaryingInput
 import ClimaUtilities.Regridders: InterpolationsRegridder
 import Interpolations
 using ClimaCore
+using NCDatasets
+using Statistics
 
 export make_set_initial_state_from_file,
     make_set_subseasonal_initial_conditions,
@@ -970,4 +972,106 @@ function set_lake_initial_conditions!(
         model.parameters,
     )
     return nothing
+end
+
+
+function make_land_ic_file_from_output_diagnostics(inpath; infilestem = "_1M_average", outfilepath = joindir(inpath, "initial_conditions.nc"))
+    attrib_swc = (;
+                  vartitle = "Volumetric fraction of water",
+                  varunits = "m^3/m^3",
+                  varname = "swc",
+                  )
+    attrib_si = (;
+                 vartitle = "Volumetric fraction of ice",
+                 varunits = "m^3/m^3",
+                 varname = "si",
+                 )
+    attrib_sie = (;
+                  vartitle = "Soil volumetric internal energy",
+                  varunits = "J/m^3",
+                  varname = "sie",
+                  )
+    attrib_swe = (;
+                  vartitle = "Snow water equivalent",
+                  varunits = "m",
+                  varname = "swe",
+                  )
+    attrib_lwp = (;
+                  vartitle = "Leaf water potential",
+                  varunits = "m",
+                  varname = "lwp",
+                  )
+    
+    var_3d_attribs = [attrib_swc, attrib_si, attrib_sie]
+    var_2d_attribs = [attrib_swe, attrib_lwp]
+    function replace_nan_with_mean!(x)
+        nan_mask = isnan.(x)
+        nonnan_mean = mean(x[.~nan_mask])
+        x[nan_mask] .= nonnan_mean
+        return nothing
+    end
+    
+    ds = NCDataset(outfilepath, "c")    
+    
+    for i in 1:length(var_3d_attribs)
+        var_attrib = var_3d_attribs[i]
+        (vartitle, varunits, varname) = var_attrib
+        data = NCDataset(joinpath(inpath, "$(varname)$(infilestem).nc"))
+        z = data["z"][:]
+        lat = data["lat"][:]
+        lon = data["lon"][:]
+        if i == 1
+            # Define and set values for dimensions (lon, lat, z)
+            defDim(ds, "lon", length(lon))
+            defDim(ds, "lat", length(lat))
+            defDim(ds, "z", length(z))
+            la = defVar(ds, "lat", Float32, ("lat",))
+            lo = defVar(ds, "lon", Float32, ("lon",))
+            zv = defVar(ds, "z", Float32, ("z",))
+            la.attrib["units"] = "degrees_north"
+            la.attrib["standard_name"] = "latitude"
+            lo.attrib["standard_name"] = "longitude"
+            lo.attrib["units"] = "degrees_east"
+            zv.attrib["standard_name"] = "depth"
+            zv.attrib["units"] = "m"
+            la[:] = lat
+            lo[:] = lon
+            zv[:] = z
+        end
+        var = defVar(ds, varname, Float32, ("lon", "lat", "z"))
+        var.attrib["units"] = varunits
+        var.attrib["longname"]= vartitle
+        var.attrib["varname"] = varname
+        field = data[varname][end,:,:,:]; # last time element
+        close(data)
+        replace_nan_with_mean!(field)
+        var[:, :, :] = field[:,:,:]
+    end
+    for i in 1:length(var_2d_attribs)
+        var_attrib = var_2d_attribs[i]
+        (vartitle, varunits, varname) = var_attrib
+        data = NCDataset(joinpath(inpath, "$(varname)$(infilestem).nc"))
+        lat = data["lat"][:]
+        lon = data["lon"][:]
+        var = defVar(ds, varname, Float32, ("lon", "lat"))
+        var.attrib["units"] = varunits
+        var.attrib["longname"]= vartitle
+        var.attrib["varname"] = varname
+        field = data[varname][end,:,:]; # last time element
+        close(data)
+        replace_nan_with_mean!(field)
+        var[:, :] = field[:,:]
+    end
+    close(ds)
+end
+
+"""
+    make_set_initial_state_from_previous_run(inpath, land::LandModel{FT}; enforce_constraints=false, infilestem = "_1M_average", outfilepath = joindir(inpath, "initial_conditions.nc")) where {FT}
+"""
+function make_set_initial_state_from_previous_run(inpath, land::LandModel{FT}; enforce_constraints=false, infilestem = "_1M_average", outfilepath = joindir(inpath, "initial_conditions.nc")) where {FT}
+    ic_path =  make_land_ic_file_from_output_diagnostics(inpath; infilestem, outfilepath)
+    set_ic! = make_set_initial_state_from_file(ic_path,
+                                               land
+                                               enforce_constraints)
+    return set_ic!
 end
