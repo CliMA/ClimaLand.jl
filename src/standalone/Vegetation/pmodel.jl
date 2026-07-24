b41cef920c598857763777eaa059c41154802b7b
nicobartolomeo <nicolab2@umbc.edu>
2026-07-24 16:25:47 -0700

    Revert "Update pmodel.jl"
    This reverts commit 4e393f52508001c1b2aa2a957ce9df1f5903fe43.

--- src/standalone/Vegetation/pmodel.jl
+++ src/standalone/Vegetation/pmodel.jl
@@ -1153,13 +1153,13 @@ get_An_leaf(p, m::PModel) = @. lazy(
 get_GPP(p, m::PModel) = p.canopy.photosynthesis.InstVars.GPP
 
 function get_J_over_Jmax(Y, p, canopy, m::PModel)
-    Jmax_c3, Jmax_c4 = compute_Jmax_canopy(Y, p, canopy, m) # lazy
+    Jmax_opt_c3, Jmax_opt_c4 = compute_Jmax_canopy(Y, p, canopy, m) # lazy
     J_c3, J_c4 = compute_J_canopy(Y, p, canopy, m) # lazy
     FT = eltype(m.constants)
     return @. lazy(
         blend(
-            J_c3 / max(Jmax_c3, sqrt(eps(FT))),
-            J_c4 / max(Jmax_c4, sqrt(eps(FT))),
+            J_c3 / max(Jmax_opt_c3, sqrt(eps(FT))),
+            J_c4 / max(Jmax_opt_c4, sqrt(eps(FT))),
             m.fractional_c3,
         ),
     )
