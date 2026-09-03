45d84eeb71f492a47406915b32e03a8f6428f2e9
kmdeck <kdeck@caltech.edu>
2026-09-03 10:56:47 -0700

    fix resid flux and change params [ skip ci]

--- src/integrated/land.jl
+++ src/integrated/land.jl
@@ -1221,11 +1221,17 @@ NVTX.@annotate function snow_boundary_fluxes!(
         P_snow * (1 - p.lake_fraction) +
         (P_liq + p.snow.turbulent_fluxes.vapor_flux - p.snow.water_runoff) *
         p.snow.snow_cover_fraction
+    residual_melt_flux = Snow.get_residual_surface_flux(
+        model.parameters.surf_temp,
+        Y,
+        p,
+        model.parameters.earth_param_set,
+    )
 
     @. p.snow.liquid_water_flux =
         (
             P_liq + p.snow.turbulent_fluxes.vapor_flux * p.snow.q_l -
-            p.snow.water_runoff
+            p.snow.water_runoff + residual_melt_flux
         ) * p.snow.snow_cover_fraction
 
     e_flux_falling_snow = Snow.energy_flux_falling_snow(
@@ -1239,13 +1245,10 @@ NVTX.@annotate function snow_boundary_fluxes!(
         model.parameters.earth_param_set,
     )
 
-    residual_surface_flux =
-        Snow.get_residual_surface_flux(model.parameters.surf_temp, Y, p)
     # positive fluxes are TOWARDS atmos, but R_n positive if snow absorbs energy
     @. p.snow.total_energy_flux =
         e_flux_falling_snow * (1 - p.lake_fraction) +
         (
-            residual_surface_flux +
             p.snow.turbulent_fluxes.lhf +
             p.snow.turbulent_fluxes.shf +
             p.snow.R_n - p.snow.energy_runoff - p.ground_heat_flux +
